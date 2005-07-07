reactos/reactos-deprecated-gitsvn-dont-use

commit 2c9102d8bda3e59c6262c8a49e6d1f1416f9936a

Generating relocations twice for DLLs? Oh, what a clever idea ... not.
git-svn-id: file:///srv/svn/reactos/trunk@16449 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/tools/rbuild/backend/mingw/modulehandler.cpp
@@ -1341,39 +1341,16 @@ MingwModuleHandler::GenerateLinkerCommand (
 
 	if ( module.IsDLL () )
 	{
-		string base_tmp = ros_temp + module.name + ".base.tmp";
-		CLEAN_FILE ( base_tmp );
-		string junk_tmp = ros_temp + module.name + ".junk.tmp";
-		CLEAN_FILE ( junk_tmp );
 		string temp_exp = ros_temp + module.name + ".temp.exp";
 		CLEAN_FILE ( temp_exp );
 	
-		fprintf ( fMakefile,
-		          "\t%s %s -Wl,--base-file,%s -o %s %s %s %s\n",
-		          linker.c_str (),
-		          linkerParameters.c_str (),
-		          base_tmp.c_str (),
-		          junk_tmp.c_str (),
-		          objectsMacro.c_str (),
-		          libsMacro.c_str (),
-		          GetLinkerMacro ().c_str () );
-	
-		fprintf ( fMakefile,
-		          "\t-@${rm} %s 2>$(NUL)\n",
-		          junk_tmp.c_str () );
-	
 		string killAt = module.mangledSymbols ? "" : "--kill-at";
 		fprintf ( fMakefile,
-		          "\t${dlltool} --dllname %s --base-file %s --def %s --output-exp %s %s\n",
+		          "\t${dlltool} --dllname %s --def %s --output-exp %s %s\n",
 		          targetName.c_str (),
-		          base_tmp.c_str (),
 		          def_file.c_str (),
 		          temp_exp.c_str (),
 		          killAt.c_str () );
-	
-		fprintf ( fMakefile,
-		          "\t-@${rm} %s 2>$(NUL)\n",
-		          base_tmp.c_str () );
 	
 		fprintf ( fMakefile,
 		          "\t%s %s %s -o %s %s %s %s\n",

commit 1deeac0d00a2918989099fc5e753d2b4762e4c66

Fix the problems introduced in r16449 by changing "-mdll --dll" to "-shared" and letting the linker generate the relocations in all cases.
git-svn-id: file:///srv/svn/reactos/trunk@16478 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/tools/rbuild/backend/mingw/modulehandler.cpp
@@ -647,7 +647,7 @@ MingwModuleHandler::GenerateMacro (
 	if ( use_pch && module.pch != NULL )
 	{
 		fprintf ( fMakefile,
-		          " -I %s",
+		          " -I%s",
 		          GetDirectory ( GetPrecompiledHeaderFilename () ).c_str () );
 	}
 
@@ -2086,7 +2086,7 @@ MingwKernelModuleHandler::GenerateKernelModuleTarget ()
 
 		string dependencies = linkDepsMacro + " " + objectsMacro;
 
-		string linkerParameters = ssprintf ( "-Wl,-T,%s" SSEP "ntoskrnl.lnk -Wl,--subsystem,native -Wl,--entry,%s -Wl,--image-base,%s -Wl,--file-alignment,0x1000 -Wl,--section-alignment,0x1000 -nostartfiles -mdll --dll",
+		string linkerParameters = ssprintf ( "-Wl,-T,%s" SSEP "ntoskrnl.lnk -Wl,--subsystem,native -Wl,--entry,%s -Wl,--image-base,%s -Wl,--file-alignment,0x1000 -Wl,--section-alignment,0x1000 -nostartfiles -shared",
 		                                     module.GetBasePath ().c_str (),
 		                                     module.entrypoint.c_str (),
 		                                     module.baseaddress.c_str () );
@@ -2173,7 +2173,7 @@ MingwKernelModeDLLModuleHandler::GenerateKernelModeDLLModuleTarget ()
 
 		string dependencies = linkDepsMacro + " " + objectsMacro;
 
-		string linkerParameters = ssprintf ( "-Wl,--subsystem,native -Wl,--entry,%s -Wl,--image-base,%s -Wl,--file-alignment,0x1000 -Wl,--section-alignment,0x1000 -nostartfiles -mdll --dll",
+		string linkerParameters = ssprintf ( "-Wl,--subsystem,native -Wl,--entry,%s -Wl,--image-base,%s -Wl,--file-alignment,0x1000 -Wl,--section-alignment,0x1000 -nostartfiles -shared",
 		                                     module.entrypoint.c_str (),
 		                                     module.baseaddress.c_str () );
 		GenerateLinkerCommand ( dependencies,
@@ -2220,7 +2220,7 @@ MingwKernelModeDriverModuleHandler::GenerateKernelModeDriverModuleTarget ()
 
 		string dependencies = linkDepsMacro + " " + objectsMacro;
 
-		string linkerParameters = ssprintf ( "-Wl,--subsystem,native -Wl,--entry,%s -Wl,--image-base,%s -Wl,--file-alignment,0x1000 -Wl,--section-alignment,0x1000 -nostartfiles -mdll --dll",
+		string linkerParameters = ssprintf ( "-Wl,--subsystem,native -Wl,--entry,%s -Wl,--image-base,%s -Wl,--file-alignment,0x1000 -Wl,--section-alignment,0x1000 -nostartfiles -shared",
 		                                     module.entrypoint.c_str (),
 		                                     module.baseaddress.c_str () );
 		GenerateLinkerCommand ( dependencies,
@@ -2266,7 +2266,7 @@ MingwNativeDLLModuleHandler::GenerateNativeDLLModuleTarget ()
 
 		string dependencies = linkDepsMacro + " " + objectsMacro;
 
-		string linkerParameters = ssprintf ( "-Wl,--subsystem,native -Wl,--entry,%s -Wl,--image-base,%s -Wl,--file-alignment,0x1000 -Wl,--section-alignment,0x1000 -nostartfiles -nostdlib -mdll --dll",
+		string linkerParameters = ssprintf ( "-Wl,--subsystem,native -Wl,--entry,%s -Wl,--image-base,%s -Wl,--file-alignment,0x1000 -Wl,--section-alignment,0x1000 -nostartfiles -nostdlib -shared",
 		                                     module.entrypoint.c_str (),
 		                                     module.baseaddress.c_str () );
 		GenerateLinkerCommand ( dependencies,
@@ -2364,7 +2364,7 @@ MingwWin32DLLModuleHandler::GenerateWin32DLLModuleTarget ()
 		else
 			linker = "${gcc}";
 
-		string linkerParameters = ssprintf ( "-Wl,--subsystem,console -Wl,--entry,%s -Wl,--image-base,%s -Wl,--file-alignment,0x1000 -Wl,--section-alignment,0x1000 -mdll --dll",
+		string linkerParameters = ssprintf ( "-Wl,--subsystem,console -Wl,--entry,%s -Wl,--image-base,%s -Wl,--file-alignment,0x1000 -Wl,--section-alignment,0x1000 -shared",
 		                                     module.entrypoint.c_str (),
 		                                     module.baseaddress.c_str () );
 		GenerateLinkerCommand ( dependencies,