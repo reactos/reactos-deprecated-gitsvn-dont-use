475cdcb7bea42914ce738214e2c1fe5f8d335cb6
gvg <gvg@9874b476-3cf2-4131-8f6d-dcf341c5f767>
2005-10-21 17:00:39 +0000

    mlsearch database import scripts by Michael Wirth
    git-svn-id: file:///home/aleksey/repos/reactos_mirr/trunk@18656 9874b476-3cf2-4131-8f6d-dcf341c5f767

--- web/reactos.org/scripts/daily.sh
+++ web/reactos.org/scripts/daily.sh
@@ -5,3 +5,6 @@ cd /web/reactos.org/scripts
 ./update_source.sh
 cd ../source/reactos
 perl ../../scripts/RosResCheck.pl > ../../htdocs/generated/transstatus.html
+cd ../../scripts
+
+./update_mlsearch_db.sh
