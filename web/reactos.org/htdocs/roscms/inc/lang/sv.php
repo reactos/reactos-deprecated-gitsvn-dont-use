ebe6632463417a9fe72ab0b650f73f2f673a8611
greatlrd <greatlrd@9874b476-3cf2-4131-8f6d-dcf341c5f767>
2005-12-16 09:46:41 +0000

    swedish translations of roscms
    git-svn-id: file:///home/aleksey/repos/reactos_mirr/trunk@20206 9874b476-3cf2-4131-8f6d-dcf341c5f767

---
+++ web/reactos.org/htdocs/roscms/inc/lang/sv.php
@@ -0,0 +1,77 @@
+ <?php
+    /*
+    RosCMS - ReactOS Content Management System
+    Copyright (C) 2005  Klemens Friedl <frik85@reactos.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    */
+	
+
+//
+// The format of this file is ---> $roscms_langres['message'] = 'text';
+//
+
+
+// Language settings
+$roscms_langres['charset'] = 'utf-8';
+//$roscms_langres['charset'] = 'iso-8859-1';
+$roscms_langres['lang_code'] = 'SV';
+
+			   
+// main menu
+$roscms_langres['Navigation'] = 'Navigation';
+$roscms_langres['Home'] = 'Hem';
+$roscms_langres['Dev'] = 'Utveckling';
+$roscms_langres['Community'] = 'Community';
+$roscms_langres['Support'] = 'Support';
+$roscms_langres['myReactOS'] = 'mittReactOS';
+
+// Login bar:
+$roscms_langres['Account'] = 'Konto';
+$roscms_langres['Login'] = 'Logga in';
+$roscms_langres['Logout'] = 'Logga ut';
+$roscms_langres['Global_Login_System'] = 'Globalt Logg in System';
+$roscms_langres['Register_Account'] = 'Registrera Konto';
+$roscms_langres['Login_Nick'] = '&nbsp;Nick:&nbsp;';
+$roscms_langres['LoginPwd'] = '&nbsp;L&ouml;senord:&nbsp;';
+
+
+// myReactOS menu
+$roscms_langres['User_Profil_Overview'] = 'Medlems profil Information';
+$roscms_langres['User_Profil'] = 'Medlems profil';
+$roscms_langres['Overview'] = '&Ouml;verblick';
+$roscms_langres['Login_System'] = 'Logg in System';
+$roscms_langres['Messages'] = 'Meddelande';
+$roscms_langres['Contacts'] = 'Kontakt';
+$roscms_langres['Favorites'] = 'Favoriter';
+$roscms_langres['Documentation'] = 'Documentation';
+$roscms_langres['Account_Edit'] = 'Inst&auml;llningar';
+$roscms_langres['Accountlist'] = 'Medlemslista';
+
+
+// myReactOS Content
+$roscms_langres['myReactOS_description'] = 'Ett kraftfult och l&auml;tts&auml;tt f&ouml;r hantera ditt ReactOS medlemskap konto och hemsidan.';
+$roscms_langres['Account_Information'] = 'Konto Information';
+$roscms_langres['Account_Information_description'] = 'Sammanst&auml;llning av konto information ... &lt;placeholder&gt;';
+
+// Right side bar:
+$roscms_langres['Latest_Release'] = 'Senaste Utg&aring;van';
+$roscms_langres['myReactOS_Developer_Quotes'] = 'Utvecklarna Citat';
+
+// Error 404
+$roscms_langres['Page_not_found'] = 'Sidan hittades inte';
+
+
+?>
