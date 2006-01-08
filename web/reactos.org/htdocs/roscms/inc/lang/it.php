e3a3d404d8ea3de92bd196c2192057c1dedb8e5a
mbosma <mbosma@97493ccd-5924-5043-b1f5-66cb403b36ce>
2006-01-08 11:29:19 +0000

    Speekix (speekix(at)gmail(dot)com): Italian language file for RosCMS (Bug 1252)
    git-svn-id: file:///srv/svn/reactos/trunk@20709 97493ccd-5924-5043-b1f5-66cb403b36ce

---
+++ web/reactos.org/htdocs/roscms/inc/lang/it.php
@@ -0,0 +1,77 @@
+<?php
+    /*
+    RosCMS - ReactOS Content Management System
+    Copyright (C) 2005  Klemens Friedl <frik85@reactos.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    */
+	
+
+//
+// The format of this file is ---> $roscms_langres['message'] = 'text';
+//
+
+
+// Language settings
+$roscms_langres['charset'] = 'utf-8';
+//$roscms_langres['charset'] = 'iso-8859-1';
+$roscms_langres['lang_code'] = 'IT';
+
+			   
+// main menu
+$roscms_langres['Navigation'] = 'Navigazione';
+$roscms_langres['Home'] = 'Home';
+$roscms_langres['Dev'] = 'Sviluppo';
+$roscms_langres['Community'] = 'Community';
+$roscms_langres['Support'] = 'Supporto';
+$roscms_langres['myReactOS'] = 'myReactOS';
+
+// Login bar:
+$roscms_langres['Account'] = 'Account';
+$roscms_langres['Login'] = 'Login';
+$roscms_langres['Logout'] = 'Logout';
+$roscms_langres['Global_Login_System'] = 'Global Login System';
+$roscms_langres['Register_Account'] = 'Registerazione Account';
+$roscms_langres['Login_Nick'] = '&nbsp;Nick:&nbsp;';
+$roscms_langres['LoginPwd'] = '&nbsp;Pwd:&nbsp;';
+
+
+// myReactOS menu
+$roscms_langres['User_Profil_Overview'] = 'Descrizione Profilo Utente';
+$roscms_langres['User_Profil'] = 'Profilo Utente';
+$roscms_langres['Overview'] = 'Descrizione';
+$roscms_langres['Login_System'] = 'Login System';
+$roscms_langres['Messages'] = 'Messaggi';
+$roscms_langres['Contacts'] = 'Contatti';
+$roscms_langres['Favorites'] = 'Preferiti';
+$roscms_langres['Documentation'] = 'Documentazione';
+$roscms_langres['Account_Edit'] = 'Impostazioni';
+$roscms_langres['Accountlist'] = 'Lista Utenti';
+
+
+// myReactOS Content
+$roscms_langres['myReactOS_description'] = 'Un'interfaccia potente e facile da usare per gestire il tuo ReactOS homepage account.';
+$roscms_langres['Account_Information'] = 'Informazioni Account';
+$roscms_langres['Account_Information_description'] = 'Un riassunto delle informazioni sul tuo account  ... &ltda supportare&gt;';
+
+// Right side bar:
+$roscms_langres['Latest_Release'] = 'Ultime Release';
+$roscms_langres['myReactOS_Developer_Quotes'] = 'Citazioni degli sviluppatori';
+
+// Error 404
+$roscms_langres['Page_not_found'] = 'Pagina non trovata';
+
+
+?>
