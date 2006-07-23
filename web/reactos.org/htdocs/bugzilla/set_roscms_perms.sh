d61301578c6e4d1487299453e35a44c586fcf287
mwirth <mwirth@97493ccd-5924-5043-b1f5-66cb403b36ce>
2006-07-23 12:46:01 +0000

    Delete old bugzilla files
    git-svn-id: file:///srv/svn/reactos/trunk@23243 97493ccd-5924-5043-b1f5-66cb403b36ce

--- web/reactos.org/htdocs/bugzilla/set_roscms_perms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-chown -R reactos.roscms .
-find . -exec chmod g+w,o+r \{\} \;
-find . -name "*.cgi" -exec chmod o+x \{\} \;
-find . -name "*.pl" -exec chmod o+x \{\} \;
-find . -name "*.sh" -exec chmod o+x \{\} \;
-find . -type d -exec chmod o+rx \{\} \;
-find . -type d -exec chmod g+s \{\} \;
-chown -R apache graphs data
