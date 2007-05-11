f65900696147de515047a393a090562fe7fadbf5
gedmurphy <gedmurphy@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-05-11 11:18:21 +0000

    - fix possible exceptions for incorrect paths - set Main as STA to allow debuging
    git-svn-id: file:///srv/svn/reactos/trunk@26694 97493ccd-5924-5043-b1f5-66cb403b36ce

--- tools/Qemu GUI/program.cs
+++ tools/Qemu GUI/program.cs
@@ -9,7 +9,7 @@ namespace Qemu_GUI
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [MTAThread]
+        [STAThread]
         static void Main()
         {
             Application.Run(new MainForm());
