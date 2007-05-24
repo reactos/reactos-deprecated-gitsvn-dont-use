1cf7843f709b6d71c88fa2ddd23a3d6aff87a9ac
gedmurphy <gedmurphy@9874b476-3cf2-4131-8f6d-dcf341c5f767>
2007-05-24 20:48:20 +0000

    add a tool to allow the RosBE cmd window to flash when the build has stopped.
    git-svn-id: file:///home/aleksey/repos/reactos_mirr/trunk@26881 9874b476-3cf2-4131-8f6d-dcf341c5f767

---
+++ tools/RosBE-Windows/Tools/flash.c
@@ -0,0 +1,47 @@
+#include <windows.h>
+
+HWND GetConsoleHwnd(void)
+{
+    WCHAR szNewTitle[MAX_PATH];
+    WCHAR szOldTitle[MAX_PATH];
+    HWND hwnd = NULL;
+
+    if (GetConsoleTitleW(szOldTitle, MAX_PATH))
+    {
+        wsprintfW(szNewTitle,
+                  L"%d/%d",
+                  GetTickCount(),
+                  GetCurrentProcessId());
+
+        if (SetConsoleTitleW(szNewTitle))
+        {
+            Sleep(40);
+
+            hwnd = FindWindowW(NULL, szNewTitle);
+
+            SetConsoleTitleW(szOldTitle);
+        }
+    }
+
+    return hwnd;
+}
+
+
+int main()
+{
+    HWND hwnd;
+    FLASHWINFO fwi = {0,};
+
+    hwnd = GetConsoleHwnd();
+    if (hwnd)
+    {
+        fwi.cbSize = sizeof(FLASHWINFO);
+        fwi.hwnd = hwnd;
+        fwi.dwFlags = FLASHW_ALL | FLASHW_TIMERNOFG;
+        fwi.uCount = 20;
+
+        FlashWindowEx(&fwi);
+    }
+
+    return 0;
+}
