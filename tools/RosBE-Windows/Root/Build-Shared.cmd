b1144788bdea4989201e2a0eb502f753ef6b7599
dreimer <dreimer@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-06-09 20:03:02 +0000

    - Whitespace trim in cpucount.c - Added links to all needed Tools to info.txt - Fixed my own stupidness once more... parameters for make got skipped...
    git-svn-id: file:///srv/svn/reactos/trunk@27091 97493ccd-5924-5043-b1f5-66cb403b36ce

--- tools/RosBE-Windows/Root/Build-Shared.cmd
+++ tools/RosBE-Windows/Root/Build-Shared.cmd
@@ -11,7 +11,7 @@ title Building...
 :: if it is then abort the build and inform the user.
 ::
 if exist "%_ROSSOURCEDIR%\config.rbuild" (
-    "%ROSBEBASEDIR%\Tools\test.exe" "%_ROSSOURCEDIR%\config.template.rbuild" -nt "%_ROSSOURCEDIR%\config.rbuild"
+    %ROSBEBASEDIR%\Tools\test.exe "%_ROSSOURCEDIR%\config.template.rbuild" -nt "%_ROSSOURCEDIR%\config.rbuild"
     if not errorlevel 1 (
         echo.
         echo *** config.template.rbuild is newer than config.rbuild ***
@@ -26,16 +26,13 @@ if exist "%_ROSSOURCEDIR%\config.rbuild" (
 :: Get the current date and time for use in
 :: in our build log's file name.
 ::
-call %ROSBEBASEDIR%\TimeDate.cmd
+call "%ROSBEBASEDIR%\TimeDate.cmd"
 
 ::
-:: Execute the Build command
+:: Now use mingw32-make to build ReactOS, passing along options, if any.
 ::
-if "%1" == "" (
-    %ROSBEBASEDIR%\Tools\buildtime.exe %_MINGWMAKE% 2>&1 | %ROSBEBASEDIR%\Tools\tee.exe %_ROSBELOGDIR%\BuildLog-%_MINGWVERSION%-%DATENAME%-%TIMENAME%.txt
-) else (
-    %ROSBEBASEDIR%\Tools\buildtime.exe %_MINGWMAKE% %* 2>&1 | %ROSBEBASEDIR%\Tools\tee.exe %_ROSBELOGDIR%\BuildLog-%_MINGWVERSION%-%DATENAME%-%TIMENAME%.txt
-)
+
+call "%ROSBEBASEDIR%\Tools\buildtime.exe" %MAKE_COMMAND% 2>&1 | "%ROSBEBASEDIR%\Tools\tee.exe" "%_ROSBELOGDIR%\BuildLog-%_MINGWVERSION%-%DATENAME%-%TIMENAME%.txt"
 
 ::
 :: highlight the fact that building has ended
