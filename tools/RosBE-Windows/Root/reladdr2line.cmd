869f329d752768db4bf3e0f0cf46146308d13665
dreimer <dreimer@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-06-18 21:08:09 +0000

    - Head0rz t0 teh masses. - Fix a typo in Help.cmd
    git-svn-id: file:///srv/svn/reactos/trunk@27229 97493ccd-5924-5043-b1f5-66cb403b36ce

--- tools/RosBE-Windows/Root/reladdr2line.cmd
+++ tools/RosBE-Windows/Root/reladdr2line.cmd
@@ -1,4 +1,13 @@
+::
+:: PROJECT:     RosBE - ReactOS Build Environment for Windows
+:: LICENSE:     GPL - See COPYING in the top level directory
+:: FILE:        Root/reladdr2line.cmd
+:: PURPOSE:     Converts a value to hex and displays it
+:: COPYRIGHT:   Copyright 2007 Christoph von Wittich <Christoph_vW@reactos.org>
+::
+::
 @echo off
+
 for /f "tokens=2" %%i in ('"objdump -p %1 2>NUL | findstr ImageBase"') do SET baseaddr=0x%%i
 set /a baseaddr += 0x%2
 for /f %%i in ('"%ROSBEBASEDIR%\Tools\echoh %baseaddr%"') do set relbase=%%i
