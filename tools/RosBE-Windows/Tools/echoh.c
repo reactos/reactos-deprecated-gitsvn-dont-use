627ab2d31390bac3f66235561cf701ac64b6c540
cfinck <cfinck@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-06-18 20:54:50 +0000

    Add a "reladdr2line" tool coded by Christoph I slightly modified his batch file, so it integrates into RosBE
    git-svn-id: file:///srv/svn/reactos/trunk@27228 97493ccd-5924-5043-b1f5-66cb403b36ce

---
+++ tools/RosBE-Windows/Tools/echoh.c
@@ -0,0 +1,17 @@
+/*
+ * PROJECT:     RosBE - ReactOS Build Environment for Windows
+ * LICENSE:     GPL - See COPYING in the top level directory
+ * FILE:        Tools/echoh.c
+ * PURPOSE:     Converts a value to hex and displays it
+ * COPYRIGHT:   Copyright 2007 Christoph von Wittich <Christoph_vW@reactos.org>
+ *
+ */
+ 
+ 
+#include <stdio.h>
+ 
+int main(int argc, char** argv) {
+  if (argc == 2)
+    printf("%x", atoi(argv[1]));
+  return 0;
+}
