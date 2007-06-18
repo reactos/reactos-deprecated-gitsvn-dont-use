d320f7f472577a33e3b35866efb8e5a6ceba0129
dreimer <dreimer@9874b476-3cf2-4131-8f6d-dcf341c5f767>
2007-06-18 21:49:44 +0000

    I don't know if it is a common phenomenon, but everytime I commit stuff, I IMMEDIATELY find things to fix/change afterwards....
    - Added ability to set non, only executable or both parameters. Missing stuff will be asked for then.
    - Added Comments
    
    git-svn-id: file:///home/aleksey/repos/reactos_mirr/trunk@27231 9874b476-3cf2-4131-8f6d-dcf341c5f767

--- tools/RosBE-Windows/Root/RAddr.cmd
+++ tools/RosBE-Windows/Root/RAddr.cmd
@@ -10,17 +10,23 @@
 
 title RAddr2Line...
 
+::
+:: Receive the Parameters and decide what to do.
+::
 if "%1" == "" (
     goto :MAN
 )
 if not "%1" == "" (
     if "%2" == "" (
-    echo No Address Set.
-    goto :EOF
+        goto :AUTO1
     ) else (
-    goto :AUTO
+        goto :AUTO2
+    )
 )
 
+::
+:: If Parameters were set, parse them, if not, ask the user to add them.
+::
 :MAN
 echo Set the Path to the Executable to be examined.
 SET /P EXE=
@@ -29,6 +35,14 @@ SET /P ADDR=
 echo.
 echo.
 "%ROSBEBASEDIR%\Tools\raddr2line.exe" %EXE% %ADDR%
+goto :EOF
 
-:AUTO
+:AUTO1
+echo Set the Address you wanna analyze inside the Executable.
+SET /P ADDR=
+"%ROSBEBASEDIR%\Tools\raddr2line.exe" %1 %ADDR%
+goto :EOF
+
+:AUTO2
 "%ROSBEBASEDIR%\Tools\raddr2line.exe" %1 %2
+goto :EOF
