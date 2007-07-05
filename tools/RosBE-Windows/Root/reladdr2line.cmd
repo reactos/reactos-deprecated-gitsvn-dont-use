3b49ce28ff34425cf9a71c99bc3725349191e221
cwittich <cwittich@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-07-05 00:30:28 +0000

    -let reladdr2line use raddr2line use this tool when you get a trace without the imagebase like in <mouclass.sys:192d>
    git-svn-id: file:///srv/svn/reactos/trunk@27395 97493ccd-5924-5043-b1f5-66cb403b36ce

--- tools/RosBE-Windows/Root/reladdr2line.cmd
+++ tools/RosBE-Windows/Root/reladdr2line.cmd
@@ -11,4 +11,4 @@
 for /f "tokens=2" %%i in ('"objdump -p %1 2>NUL | findstr ImageBase"') do SET baseaddr=0x%%i
 set /a baseaddr += 0x%2
 for /f %%i in ('"%ROSBEBASEDIR%\Tools\echoh %baseaddr%"') do set relbase=%%i
-addr2line --exe=%1 %relbase% 2>NUL
+raddr2line %1 %relbase% 2>NUL
