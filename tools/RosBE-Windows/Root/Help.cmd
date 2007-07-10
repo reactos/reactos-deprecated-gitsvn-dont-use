9222398f9f2e717e9b89fcf2f4659cbbc5edf16b
dreimer <dreimer@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-07-10 12:56:57 +0000

    - Some more cleanup - Added chdefdir to Help.
    git-svn-id: file:///srv/svn/reactos/trunk@27560 97493ccd-5924-5043-b1f5-66cb403b36ce

--- tools/RosBE-Windows/Root/Help.cmd
+++ tools/RosBE-Windows/Root/Help.cmd
@@ -23,6 +23,8 @@ if "%1" == "" (
     echo                           with the appropriate number.
     echo    basedir              - Switch back to the ReactOS source directory.
     echo.
+    echo    chdefdir [OPTIONS]   - Reconfigures the Default Source Folder for one
+    echo                           Session.
     echo    config [OPTIONS]     - Configures the way, ReactOS will be built.
     echo.
     echo    clean [logs]         - Fully clean the ReactOS source directory and/or
@@ -77,6 +79,14 @@ if "%1" == "ssvn" (
     echo    cleanup - Cleans up and fixes errors in the Source Tree.
     echo    status  - Shows Actual offline Revision Number and online HEAD
     echo              Revision Number.
+    goto :EOF
+)
+if "%1" == "chdefdir" (
+    echo Usage: chdefdir [OPTIONS]
+    echo Reconfigures the Default Source Folder for one Session. A Reset sets
+    echo back to the Default.
+    echo.
+    echo    default - Sets back to the Default Source Folder without a restart.
     goto :EOF
 )
 if "%1" == "config" (
