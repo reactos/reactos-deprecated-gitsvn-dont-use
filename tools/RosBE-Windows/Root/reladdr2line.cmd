286c1b061c18bf38b6129c8d4f2a2b0538342a66
dreimer <dreimer@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-07-14 14:35:00 +0000

    Fix a typo in RosBE.cmd. Simplify RAddr.cmd. Start to modify reladdr2line.cmd With some luck its already working
    git-svn-id: file:///srv/svn/reactos/trunk@27658 97493ccd-5924-5043-b1f5-66cb403b36ce

--- tools/RosBE-Windows/Root/reladdr2line.cmd
+++ tools/RosBE-Windows/Root/reladdr2line.cmd
@@ -10,9 +10,49 @@
 
 title relAddr2Line...
 
-for /f "tokens=2" %%i in ('"objdump -p %1 2>NUL | findstr ImageBase"') do SET baseaddr=0x%%i
-set /a baseaddr += 0x%2
+set 1 = %1
+set 2 = %2
+
+::
+:: Receive the Parameters and decide what to do.
+::
+if "%1%" == "" (
+    goto :MAN
+)
+if not "%1%" == "" (
+    if "%2%" == "" (
+        goto :AUTO1
+    ) else (
+        goto :EOC
+    )
+)
+
+::
+:: If Parameters were set, parse them, if not, ask the user to add them.
+::
+:MAN
+echo Set the Path to the Executable to be examined.
+SET /P 1 =
+echo Set the Address you wanna analyze inside the Executable.
+SET /P 2 =
+echo.
+goto :EOC
+
+:AUTO1
+echo Set the Address you wanna analyze inside the Executable.
+echo.
+SET /P 2 =
+goto :EOC
+
+:EOC
+for /f "tokens=2" %%i in ('"objdump -p %1% 2>NUL | findstr ImageBase"') do SET baseaddr=0x%%i
+set /a baseaddr += 0x%2%
 for /f %%i in ('"echoh %baseaddr%"') do set relbase=%%i
-raddr2line "%1" "%relbase%" 2>NUL
+raddr2line "%1%" "%relbase%" 2>NUL
 
 title ReactOS Build Environment %_VER%
+
+::%1 Dateiname
+::%2 Adresse
+::%%i das was objdump rausfindet
+::baseaddr = %%i + Adresse
