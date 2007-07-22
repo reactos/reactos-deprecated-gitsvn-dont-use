a165a6fab4affbe264bea1aa4098f4f4fc2609be
dreimer <dreimer@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-07-22 15:55:56 +0000

    - First working try of my Fast Source Tree Switcher. - Add cutz.cmd to bypass a STUUUPIIID Bug in cut under Windows.   (=Thinking every value in a line after starting cut belongs to it.)
    git-svn-id: file:///srv/svn/reactos/trunk@27777 97493ccd-5924-5043-b1f5-66cb403b36ce

---
+++ tools/RosBE-Windows/Tools/cutz.cmd
@@ -0,0 +1,10 @@
+::
+:: PROJECT:     RosBE - ReactOS Build Environment for Windows
+:: LICENSE:     GPL - See COPYING in the top level directory
+:: FILE:        Tools/cutz.cmd
+:: PURPOSE:     Bypass a stupid bug in cut.
+:: COPYRIGHT:   Copyright 2007 Daniel Reimer <reimer.daniel@freenet.de>
+::
+::
+@echo off
+cut -d "\"" -f 4
