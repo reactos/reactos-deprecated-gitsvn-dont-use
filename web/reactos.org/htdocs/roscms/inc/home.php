6ca163937ccd39e9f7d06ec6b31b4d8e26a4fa4a
frik85 <frik85@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-08-14 15:26:16 +0000

    RosCMS update: * install-script: easy to use install-script to setup RosCMS (only PHP 4+ & MySQL 4+ are required) * up-to-date SQL-dumb file * if you want to install RosCMS on your server, try out the prepaired package: http://www.reactos.org/?page=roscms
    git-svn-id: file:///srv/svn/reactos/trunk@28339 97493ccd-5924-5043-b1f5-66cb403b36ce

--- web/reactos.org/htdocs/roscms/inc/home.php
+++ web/reactos.org/htdocs/roscms/inc/home.php
@@ -20,10 +20,15 @@
 ?>
 <?php /*require("inc/data_menu.php"); */ ?>
 <p>&nbsp;</p>
-<h2>RosCMS v2 to RosCMS v3 update</h2>
-<h4>RosCMS v2 will be replaced by RosCMS v3 in a few hours, come back later!</h4>
-<p>If you have any questions, please join #reactos-web IRC channel. See reactos.org website for more information about our IRC channels.<br  /><br  />
-Klemens Friedl, 2007-07-30 11:10 GMT+1</p>
+<h2>RosCMS v3 - Status </h2>
+<p><strong>RosCMS v2 got replaced by RosCMS v3 on 2007-07-31!</strong></p>
+<h4>Tutorials</h4>
+<p>Please read and/or watch the<strong> <a href="http://www.reactos.org/?page=tutorial_roscms">RosCMS Tutorials</a></strong> to learn more about RosCMS v3 new interface and its features! </p>
+<h4>Welcome page </h4>
+<p>Please change your RosCMS-bookmark to &quot;<a href="http://www.reactos.org/roscms/?page=data&amp;branch=welcome">http://www.reactos.org/roscms/?page=data&amp;branch=welcome</a>&quot;. The new <a href="http://www.reactos.org/roscms/?page=data&amp;branch=welcome">RosCMS Welcome-page</a> should be your first start point to get the latest information about the website translation process and anything related to website content changes. </p>
+<p>If you have any questions, please join #reactos-web IRC channel. See reactos.org website for more information about our IRC channels.<br  />
+  <br  />
+Klemens Friedl, 2007-07-31 15:42 GMT+1</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 
