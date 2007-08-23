113d4ba68a39e3225c7c4d252e7698e0edde42e7
dreimer <dreimer@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-08-23 21:24:42 +0000

    - Updated Changelog. - Modified one error to be more helpful. - Beginning of a German translation of RosBE (still missing: config, help). Do NOT translate it in another language yet. I still need to find a nice way to select the language. Heeya, Waxy :-P
    git-svn-id: file:///srv/svn/reactos/trunk@28496 97493ccd-5924-5043-b1f5-66cb403b36ce

---
+++ tools/RosBE-Windows/Root/de-DE/RosBE.cmd
@@ -0,0 +1,130 @@
+::
+:: PROJECT:     RosBE - ReactOS Build Environment for Windows
+:: LICENSE:     GPL - See COPYING in the top level directory
+:: FILE:        Root/RosBE.cmd
+:: PURPOSE:     This script provides/sets up various build environments for
+::              ReactOS. Currently it provides GCC 4.1.3 build environments.
+:: COPYRIGHT:   Copyright 2007 Daniel Reimer <reimer.daniel@freenet.de>
+::                             Peter Ward <dralnix@gmail.com>
+::
+::
+@echo off
+
+::
+:: Set Defaults to work with and override them if edited by
+:: Config Tool.
+::
+color 0A
+set _VER=0.3.7.2
+set ROSBEBASEDIR=%~dp0
+for /f "usebackq" %%i in (`"echo %ROSBEBASEDIR%|%ROSBEBASEDIR%Tools\sed s/.$//g"`) do @SET ROSBEBASEDIR=%%i
+set _ROSSOURCEDIRBASE=%CD%
+set ROSBE_SHOWTIME=1
+set ROSBE_WRITELOG=1
+set _LOGDIR=%CD%\RosBE-Logs
+
+if exist %ROSBEBASEDIR%\options.cmd (
+    goto :PREV
+) else (
+    set logdiff=0
+    goto :NEXT
+)
+
+:PREV
+call %ROSBEBASEDIR%\options.cmd
+if %_LOGDIR% == %ROSBE_LOGPATH% (
+    set logdiff=0
+) else (
+    set logdiff=1
+)
+
+:NEXT
+title ReactOS Build Environment %_VER%
+
+::
+:: Make sure RosBE was initialized right.
+::
+if "%1" == "" (
+    cls
+    call :RosBE4
+    goto :EndCommandParse
+)
+if not "%1" == "" (
+    cls
+    echo Unbekannter Parameter. Anwendung wird geschlossen.
+    goto :ExitRosBE
+)
+
+:EndCommandParse
+
+::
+:: Load the Base Directory from the Shortcut-XML and set it as new
+:: Source Directory and add PATH for Tools Folder.
+::
+set PATH=%ROSBEBASEDIR%\Tools;%PATH%
+for /f "usebackq" %%i in (`"grep base= "%ROSBEBASEDIR%\srclist.xml"|cutz main"`) do @SET scut=%%i
+echo Verwendeter Shortcut: %scut%
+echo.
+
+::
+:: Display the current version of GCC, NASM, ld and make.
+::
+gcc -v 2>&1 | find "gcc version"
+nasm -v
+ld -v
+mingw32-make -v | find "GNU Make"
+
+::
+:: Tell how to display the available commands.
+::
+echo.
+echo Für eine Liste aller integrierten Befehle, tippen Sie: "help" ein.
+echo ------------------------------------------------------------------
+
+::
+:: Load the macros that serve as our commands and set new Source
+:: Directory / Default Source Directory.
+::
+doskey /macrofile="%ROSBEBASEDIR%\RosBE.mac"
+if "%scut%" == "Default" (
+    set _ROSSOURCEDIR=%_ROSSOURCEDIRBASE%
+) else (
+    call %ROSBEBASEDIR%\scut %scut%
+)
+::
+:: Look if the Source Folder is empty. If so, ask for using "svn create".
+::
+dir /b "%_ROSSOURCEDIR%" 2>nul|findstr "." >nul
+if errorlevel 1 (
+    echo Kein ReactOS Source gefunden. Bitte nutzen Sie "ssvn create", um ihn herunterzuladen.
+    goto :ExitRosBE
+) else (
+    goto :ExitRosBE
+)
+goto :ExitRosBE
+
+::
+:: Display the banner and set up the environment for the GCC 4.x.x build
+:: environment.
+::
+:RosBE4
+    ::
+    :: Set the correct path for the build tools and set the MinGW make.
+    ::
+    set PATH=%ROSBEBASEDIR%\4.1.3\bin;%ROSBEBASEDIR%\4.1.3\libexec\gcc\mingw32\4.1.3;%PATH%
+    set _MINGWMAKE=%ROSBEBASEDIR%\4.1.3\bin\mingw32-make.exe
+    set _MINGWVERSION=4.1.3
+    :: Set all paths (needed by Windows Vista)
+    set C_INCLUDE_PATH=%ROSBEBASEDIR%\4.1.3\include;%ROSBEBASEDIR%\4.1.3\lib\gcc\mingw32\4.1.3\include
+    set CPLUS_INCLUDE_PATH=%ROSBEBASEDIR%\4.1.3\include;%ROSBEBASEDIR%\4.1.3\include\c++\4.1.3;%ROSBEBASEDIR%\4.1.3\include\c++\4.1.3\mingw32;%ROSBEBASEDIR%\4.1.3\lib\gcc\mingw32\4.1.3\include
+    set LIBRARY_PATH=%ROSBEBASEDIR%\4.1.3\lib;%ROSBEBASEDIR%\4.1.3\lib\gcc\mingw32\4.1.3
+
+    echo *******************************************************************************
+    echo *                                                                             *
+    echo *        ReactOS Build Environment %_VER%-4.1.3-20070703 (prerelease)        *
+    echo *                                                                             *
+    echo *******************************************************************************
+    echo.
+goto :EOF
+
+:ExitRosBE
