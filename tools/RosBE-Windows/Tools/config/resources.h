b73edf55be2fde83ae24d182dc55821c4e9a47c2
dreimer <dreimer@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-09-30 13:56:28 +0000

    Patch III from Pierre Schweitzer. Correct Button States and Whitespace changes.
    git-svn-id: file:///srv/svn/reactos/trunk@29311 97493ccd-5924-5043-b1f5-66cb403b36ce

--- tools/RosBE-Windows/Tools/config/resources.h
+++ tools/RosBE-Windows/Tools/config/resources.h
@@ -1,12 +1,13 @@
-#define ID_OPTICON 0x3
-#define ID_DIALOG 0x10
 #define ID_OK 0x1
 #define ID_CANCEL 0x2
-#define IDC_FONT 0x1
-#define IDC_BACK 0x2
-#define ID_SAVELOGS 0x5
-#define ID_SHOWBUILDTIME 0x6
-#define ID_LOGDIR 0x7
+#define ID_OPTICON 0x3
+#define ID_SAVELOGS 0x4
+#define ID_SHOWBUILDTIME 0x5
+#define ID_LOGDIR 0x6
 #define ID_BROWSE 0x7
 #define ID_MGWDIR 0x8
 #define ID_BROWSEMGW 0x9
+#define ID_DIALOG 0x10
+#define IDC_FONT 0x1
+#define IDC_BACK 0x2
+
