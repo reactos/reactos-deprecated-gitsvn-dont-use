reactos/reactos-deprecated-gitsvn-dont-use

commit f21a73592c40c9b59ffff3c7038c20211849593b

- fix a bug - fix identation - only register hotkeys when not being in setup
See issue #2684 for more details.


git-svn-id: file:///home/aleksey/repos/reactos_mirr/trunk@29644 9874b476-3cf2-4131-8f6d-dcf341c5f767

reactos/base/system/winlogon/sas.c
@@ -267,9 +267,9 @@ cleanup:
 	RemoveStatusMessage(Session);
 	if (!ret)
 	{
-	    Session->UserToken = NULL;
-	    CloseHandle(Session->UserToken);
-    }
+		CloseHandle(Session->UserToken);
+		Session->UserToken = NULL;
+	}
 	return ret;
 }
 
@@ -869,8 +869,10 @@ SASWindowProc(
 
 			/* Save the Session pointer */
 			SetWindowLongPtrW(hwndDlg, GWLP_USERDATA, (LONG_PTR)Session);
-
-			return RegisterHotKeys(Session, hwndDlg);
+			if (!GetSetupType())
+			{
+				return RegisterHotKeys(Session, hwndDlg);
+			}
 		}
 		case WM_DESTROY:
 		{

commit 0ced17958494bb3b9978ca04f36d02157c2002d7

Better resource freeing Fix a bug introduced in r29644, when WM_CREATE was sometimes leaking to WM_DESTROY
git-svn-id: file:///home/aleksey/repos/reactos_mirr/trunk@29651 9874b476-3cf2-4131-8f6d-dcf341c5f767

reactos/base/system/winlogon/sas.c
@@ -220,13 +220,8 @@ HandleLogon(
 	}
 	/* FIXME: Append variables of Session->Profile->pszEnvironment */
 
-	//DisplayStatusMessage(Session, Session->WinlogonDesktop, IDS_APPLYINGYOURPERSONALSETTINGS);
-	/* FIXME: UpdatePerUserSystemParameters(0, TRUE); */
-
-	/* Get privilege */
-	/* FIXME: who should do it? winlogon or gina? */
-	/* FIXME: reverting to lower privileges after creating user shell? */
-	RtlAdjustPrivilege(SE_ASSIGNPRIMARYTOKEN_PRIVILEGE, TRUE, FALSE, &Old);
+	DisplayStatusMessage(Session, Session->WinlogonDesktop, IDS_APPLYINGYOURPERSONALSETTINGS);
+	UpdatePerUserSystemParameters(0, TRUE);
 
 	/* Set default language */
 	if (!SetDefaultLanguage(TRUE))
@@ -234,6 +229,11 @@ HandleLogon(
 		WARN("WL: SetDefaultLanguage() failed\n");
 		goto cleanup;
 	}
+
+	/* Get privilege */
+	/* FIXME: who should do it? winlogon or gina? */
+	/* FIXME: reverting to lower privileges after creating user shell? */
+	RtlAdjustPrivilege(SE_ASSIGNPRIMARYTOKEN_PRIVILEGE, TRUE, FALSE, &Old);
 
 	if (!Session->Gina.Functions.WlxActivateUserShell(
 		Session->Gina.Context,
@@ -255,7 +255,11 @@ HandleLogon(
 	ret = TRUE;
 
 cleanup:
-	HeapFree(GetProcessHeap(), 0, Session->Profile);
+	if (Session->Profile)
+	{
+		HeapFree(GetProcessHeap(), 0, Session->Profile->pszProfile);
+		HeapFree(GetProcessHeap(), 0, Session->Profile);
+	}
 	Session->Profile = NULL;
 	if (!ret
 	 && ProfileInfo.hProfile != INVALID_HANDLE_VALUE)
@@ -487,9 +491,9 @@ HandleLogoff(
 		return STATUS_UNSUCCESSFUL;
 	}
 
-	//UnloadUserProfile(Session->UserToken, Session->hProfileInfo);
-	//CloseHandle(Session->UserToken);
-	//UpdatePerUserSystemParameters(0, FALSE);
+	UnloadUserProfile(Session->UserToken, Session->hProfileInfo);
+	CloseHandle(Session->UserToken);
+	UpdatePerUserSystemParameters(0, FALSE);
 	Session->LogonStatus = WKSTA_IS_LOGGED_OFF;
 	Session->UserToken = NULL;
 	return STATUS_SUCCESS;
@@ -869,10 +873,9 @@ SASWindowProc(
 
 			/* Save the Session pointer */
 			SetWindowLongPtrW(hwndDlg, GWLP_USERDATA, (LONG_PTR)Session);
-			if (!GetSetupType())
-			{
-				return RegisterHotKeys(Session, hwndDlg);
-			}
+			if (GetSetupType() == 0)
+				return TRUE;
+			return RegisterHotKeys(Session, hwndDlg);
 		}
 		case WM_DESTROY:
 		{