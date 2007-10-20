f391944f09190a4eb549afaecb16905fefa118d7
peterw <peterw@9874b476-3cf2-4131-8f6d-dcf341c5f767>
2007-10-20 01:31:21 +0000

    - Merge the three build.cmd files into one and start cleaning up the .cmd files, unfinished.
    git-svn-id: file:///home/aleksey/repos/reactos_mirr/trunk@29695 9874b476-3cf2-4131-8f6d-dcf341c5f767

--- tools/RosBE-Windows/Root/Build.cmd
+++ tools/RosBE-Windows/Root/Build.cmd
@@ -13,13 +13,135 @@
 title Building...
 
 ::
-:: Set the build command
+:: Check if the user has used the options utility and
+:: if so, load their options.
 ::
-set MAKE_COMMAND="%_MINGWMAKE%" %1
+if exist %ROSBEBASEDIR%\options.cmd (
+    call %ROSBEBASEDIR%\options.cmd
+)
 
 ::
-:: Execute the shared build commands
+:: Check if config.template.rbuild is newer than config.rbuild, if it is then
+:: abort the build and inform the user.
 ::
-call "%ROSBEBASEDIR%\Build-Shared.cmd" %2 %3
+if exist "config.rbuild" (
+    test "config.template.rbuild" -nt "config.rbuild"
+    if not errorlevel 1 (
+        echo.
+        echo *** config.template.rbuild is newer than config.rbuild ***
+        echo *** aborting build. Please update your config.rbuild   *** 
+        echo *** by typeing "config update".                        ***
+        echo.
+        goto :EOB
+    )
+)
+
+::
+:: Clear variables before use.
+::
+set CPUCOUNT=
+
+::
+:: Get the number of CPUs in the system so we know how many jobs to execute.
+:: To modify the number used alter the options used with cpucount:
+:: No Option - Number of CPUs.
+:: -x1       - Number of CPUs, Plus 1.
+:: -x2       - Number of CPUs, Doubled.
+::
+for /f "usebackq" %%i in (`cpucount -x1`) do set CPUCOUNT=%%i
+
+::
+:: Check if strip or ccache are being used and set the appropriate options.
+::
+if defined ROSBE_STRIP (
+    if %ROSBE_STRIP% == 1 (
+        set ROS_LEAN_AND_MEAN=yes
+    ) else (
+        set ROS_LEAN_AND_MEAN=no
+    )
+)
+if defined ROSBE_USECCACHE (
+    if %ROSBE_USECCACHE% == 1 (
+        set HOST_CC=ccache gcc
+        set HOST_CPP=ccache g++
+        set TARGET_CC=ccache gcc
+        set TARGET_CPP=ccache g++
+    ) else (
+        set HOST_CC=gcc
+        set HOST_CPP=g++
+        set TARGET_CC=gcc
+        set TARGET_CPP=g++
+    )
+)
+
+::
+:: Get the current date and time for use in in our build log's file name.
+::
+call "%ROSBEBASEDIR%\TimeDate.cmd"
+
+::
+:: Check if writing logs is enabled, if so check if our log directory
+:: exists, if it doesn't, create it, finally if creating the
+:: directory fails then fall back on the current directory.
+::
+if %ROSBE_WRITELOG% == 1 (
+    if not exist "_ROSBELOGDIR\." (
+        mkdir "%_ROSBELOGDIR%" 1> NUL 2> NUL
+        if errorlevel 1 (
+            echo *** Error - Writing logs requested, but log directory doesn't ***
+            echo *** exist and can't be created. Logs will be created in the   ***
+            echo *** current directory as a fallback.                          ***
+            set _ROSBELOGDIR=%CD%
+        )
+    )
+)
+
+::
+:: Check if we are using -j or not.
+::
+if "%1" == "multi" (
+    title Started: %TIMERAW%, Building...
+    goto :BUILDMULTI
+) else (
+    title Started: %TIMERAW%, Building...
+    goto :BUILD
+)
+
+:BUILD
+if %ROSBE_SHOWTIME% == 1 (
+    if %ROSBE_WRITELOG% == 1 (
+        buildtime "%_MINGWMAKE%" %* 2>&1 | tee "%_ROSBELOGDIR%\BuildLog-%_MINGWVERSION%-%DATENAME%-%TIMENAME%.txt"
+    ) else (
+        buildtime "%_MINGWMAKE%" %*
+    )
+) else (
+    if %ROSBE_WRITELOG% == 1 (
+        "%_MINGWMAKE%" %* 2>&1 | tee "%_ROSBELOGDIR%\BuildLog-%_MINGWVERSION%-%DATENAME%-%TIMENAME%.txt"
+    ) else (
+        "%_MINGWMAKE%" %*
+    )
+)
+goto :EOB
+
+:BUILDMULTI
+if %ROSBE_SHOWTIME% == 1 (
+    if %ROSBE_WRITELOG% == 1 (
+        buildtime "%_MINGWMAKE%" -j %CPUCOUNT% %2 %3 %4 %5 %6 %7 %8 %9 2>&1 | tee "%_ROSBELOGDIR%\BuildLog-%_MINGWVERSION%-%DATENAME%-%TIMENAME%.txt"
+    ) else (
+        buildtime "%_MINGWMAKE%" -j %CPUCOUNT% %2 %3 %4 %5 %6 %7 %8 %9
+    )
+) else (
+    if %ROSBE_WRITELOG% == 1 (
+        "%_MINGWMAKE%" -j %CPUCOUNT% %2 %3 %4 %5 %6 %7 %8 %9 2>&1 | tee "%_ROSBELOGDIR%\BuildLog-%_MINGWVERSION%-%DATENAME%-%TIMENAME%.txt"
+    ) else (
+        "%_MINGWMAKE%" -j %CPUCOUNT% %2 %3 %4 %5 %6 %7 %8 %9
+    )
+)
+
+:EOB
+::
+:: Highlight the fact that building has ended.
+::
+flash
 
 title ReactOS Build Environment %_VER%
