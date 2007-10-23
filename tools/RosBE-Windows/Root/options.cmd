728325a115d794218e764bee7a0606c866f68904
dreimer <dreimer@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-10-23 01:38:27 +0000

    Get rid of svnversion.exe and its need in ssvn.cmd. Now we call cut two times, but there seems to be NO way to get around it. If you try to do it without cut, then it wont load anything useful into the Variables, because the Variables are filled even before svn outputs anything. I even tried "cmd /V:ON /K" no way...
    git-svn-id: file:///srv/svn/reactos/trunk@29807 97493ccd-5924-5043-b1f5-66cb403b36ce

--- tools/RosBE-Windows/Root/options.cmd
+++ tools/RosBE-Windows/Root/options.cmd
@@ -2,7 +2,7 @@
 :: PROJECT:     RosBE - ReactOS Build Environment for Windows
 :: LICENSE:     GPL - See LICENSE.txt in the top level directory.
 :: FILE:        Root/options.cmd
-:: PURPOSE:     Starts options.exe and reboots RosBE at the End.
+:: PURPOSE:     Starts options.exe and restarts RosBE afterwards.
 :: COPYRIGHT:   Copyright 2007 Daniel Reimer <reimer.daniel@freenet.de>
 ::
 ::
