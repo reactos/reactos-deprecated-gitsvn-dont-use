b656d737cd8fdb8469cc3400b995d44e6273e244
peterw <peterw@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-10-25 10:29:36 +0000

    - Fix chdefdir.
    git-svn-id: file:///srv/svn/reactos/trunk@29873 97493ccd-5924-5043-b1f5-66cb403b36ce

--- tools/RosBE-Windows/Root/chdefdir.cmd
+++ tools/RosBE-Windows/Root/chdefdir.cmd
@@ -5,49 +5,39 @@
 :: PURPOSE:     Tool to change the current working ReactOS source directory
 ::              in RosBE.
 :: COPYRIGHT:   Copyright 2007 Daniel Reimer <reimer.daniel@freenet.de>
+::                             Peter Ward <dralnix@gmail.com>
 ::
 ::
 @echo off
 
 title Change the current working ReactOS source directory...
 
-if not %_ROSBE_PREVIOUSSOURCEDIR% == %_ROSBE_ROSSOURCEDIR% (
-    set _ROSBE_PREVIOUSSOURCEDIR=%_ROSBE_ROSSOURCEDIR%
-)
-
 ::
 :: Parse the command line arguments.
 ::
-if /i "%1" == "" (
-    goto :INTERACTIVE
+if "%1" == "" (
+    call :INTERACTIVE
+) else (
+    set _1=%1
 )
-if /i "%1" == "previous" (
+if "%_1%" == "previous" (
     set _ROSBE_ROSSOURCEDIR=%_ROSBE_PREVIOUSSOURCEDIR%
 ) else (
-    if not exist "%1\." (
+    if not exist "%_1%\." (
         echo ERROR: The path specified doesn't seem to exist.
         goto :EOC
     )
-    set _ROSBE_ROSSOURCEDIR=%1
+    set _ROSBE_PREVIOUSSOURCEDIR=%_ROSBE_ROSSOURCEDIR%
+    set _ROSBE_ROSSOURCEDIR=%_1%
 )
 goto :EOC
 
+::
+:: If Parameters were set, parse them, if not, ask the user to add them.
+::
 :INTERACTIVE
-set /p XY="Please enter a ReactOS source directory, or 'previous': "
-
-if /i "%XY%" == "" (
-    echo ERROR: You must either enter a valid directory or "previous".
-    goto :EOC
-)
-if /i "%XY%"=="previous" (
-    set _ROSBE_ROSSOURCEDIR=%_ROSBE_PREVIOUSSOURCEDIR%
-) else (
-    if not exist "%XY%\." (
-        echo ERROR: The path specified doesn't seem to exist.
-        goto :EOC
-    )
-    set _ROSBE_ROSSOURCEDIR=%XY%
-)
+set /p _1="Please enter a ReactOS source directory, or 'previous': "
+goto :EOF
 
 :EOC
 title ReactOS Build Environment %_ROSBE_VERSION%
