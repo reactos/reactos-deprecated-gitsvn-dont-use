f7c71ea44171dedf29ed5f3a52952e96f0302404
peterw <peterw@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-10-25 01:15:26 +0000

    - Added a small utility (chkslash) to check if a string contains   a backslash '\', it's needed for the reladdr2line.cmd changes   below. - Modified reladdr2line so that you can now use 'raddr2line ntdll.dll 7c90e143'   instead of 'raddr2line output-i386\dll\ntdll\ntdll.dll 7c90e143'. You can   also use part of the file name ie. 'ntdll' instead of 'ntdll.dll' and   it will match it. Searchs the current directory and all sub-directories.   (does not match directories of course)   (yes you can still specify the full path if you like) - Some other miscellaneous cleanup.
    git-svn-id: file:///srv/svn/reactos/trunk@29861 97493ccd-5924-5043-b1f5-66cb403b36ce

--- tools/RosBE-Windows/Root/reladdr2line.cmd
+++ tools/RosBE-Windows/Root/reladdr2line.cmd
@@ -5,64 +5,84 @@
 :: PURPOSE:     Converts a value to hex and displays it.
 :: COPYRIGHT:   Copyright 2007 Christoph von Wittich <Christoph_vW@reactos.org>
 ::                             Daniel Reimer <reimer.daniel@freenet.de>
+::                             Peter Ward <dralnix@gmail.com>
 ::
 ::
 @echo off
 
-title relAddr2Line...
+title reladdr2line...
+
+::
+:: Clear variables before use.
+::
+set _1=
+set _2=
 
 ::
 :: Receive the Parameters and decide what to do.
 ::
-
-if "%1" == "" (
-    goto :MAN
+if not "%3" == "" (
+    echo ERROR: Too many parameters specified.
+    goto :EOC
 )
 if not "%1" == "" (
     set _1=%1
-    if "%2" == "" (
-        goto :AUTO1
-    ) else (
-        set _2=%2
-        goto :EOC
-    )
+    call :CHECKPATH
 )
+if not "%2" == "" (
+    set _2=%2
+)
+call :INTERACTIVE
 
-::
-:: If Parameters were set, parse them, if not, ask the user to add them.
-::
-:MAN
-set /p _1="Please enter the path to the executable to be examined: "
-:AUTO1
-set /p _2="Please enter the address you would like to analyze: "
-
-:EOC
-if not exist "%_1%\." (
-    echo ERROR: The path specified doesn't seem to exist.
-    goto :END
-)
-if /i "%_1%" == "" (
-    echo ERROR: You must enter a valid directory.
-    goto :END
-)
-if /i "%_2%" == "" (
-    echo ERROR: You must enter a address to analyze.
-    goto :END
-)
+:RADD2RLINE
 ::
 :: First get the ImageBase of the File. If its smaller than the given
 :: Parameter, everything is ok, because it was already added onto the
 :: adress and can be given directly to raddr2line. If not, add it and
 :: give the result to raddr2line.
 ::
+if %_1% == "" (
+    echo ERROR: You must specify a path/file to examine.
+    goto :EOC
+)
+if not exist "%_1%\." (
+    echo ERROR: The path specified doesn't seem to exist.
+    goto :EOC
+)
 for /f "tokens=2" %%i in ('"objdump -p %_1% 2>NUL | findstr ImageBase"') do set baseaddr=0x%%i
 if %%i lss %_2% (
-    raddr2line "%_1%" "%_2%" 2>NUL
+    raddr2line "%_1%" "%_2%"
 ) else (
     set /a baseaddr+=0x%_2%
     for /f %%i in ('"echoh %baseaddr%"') do set relbase=%%i
-    raddr2line "%_1%" "%relbase%" 2>NUL
+    raddr2line "%_1%" "%relbase%"
 )
+goto :EOC
 
-:END
+::
+:: If Parameters were set, parse them, if not, ask the user to add them.
+::
+:INTERACTIVE
+if "%_1%" == "" (
+    set /p _1="Please enter the path/file to be examined: "
+    call :CHECKPATH
+)
+if "%_2%" == "" (
+    set /p _2="Please enter the address you would like to analyze: "
+)
+goto :EOF
+
+::
+:: Check if the user supplied a path, if they didn't look for
+:: the specified file in the current directory and any
+:: sub-directories.
+::
+:CHECKPATH
+    chkslash %_1%
+    if errorlevel 2 (
+        for /f "usebackq" %%i in (`"dir /a:-d /s /b %1 | findstr "%_1%""`) do set _1=%%i
+    )
+goto :EOF
+
+:EOC
 title ReactOS Build Environment %_ROSBE_VERSION%
