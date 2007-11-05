b47ba65e6bd68c1d53a0c6abf145ab717a61868b
cfinck <cfinck@9874b476-3cf2-4131-8f6d-dcf341c5f767>
2007-11-05 10:46:50 +0000

    Import the additional parameters from RosBE-Windows' cpucount into the Unix version
    git-svn-id: file:///home/aleksey/repos/reactos_mirr/trunk@30158 9874b476-3cf2-4131-8f6d-dcf341c5f767

--- tools/RosBE-Unix/tools/cpucount.c
+++ tools/RosBE-Unix/tools/cpucount.c
@@ -11,21 +11,45 @@
 #include <sys/sysctl.h>
 #endif
 
-int
-main(int argc,
-     char *argv[])
+int main(int argc, char *argv[])
 {
 	int cpuCount = 0;
+
+	if(argc > 2)
+	{
+		fprintf(stderr, "%s: Error too many parameters specified.\n", argv[0]);
+		return -1;
+	}
 
 #ifdef __APPLE__
 	size_t countSize = sizeof(cpuCount);
 
 	sysctlbyname("hw.logicalcpu", &cpuCount, &countSize, NULL, 0);
 #else
-	cpuCount = sysconf(_SC_NPROCESSORS_ONLN); 
+	cpuCount = sysconf(_SC_NPROCESSORS_ONLN);
 #endif
 
-	printf( "%u\n", cpuCount);
+	if(argc != 1)
+	{
+		if(!strncmp(argv[1], "-x1", 3))
+		{
+			cpuCount++;
+		}
+		else if(!strncmp(argv[1], "-x2", 3))
+		{
+			cpuCount += cpuCount;
+		}
+		else
+		{
+			printf("Usage: %s [OPTIONS]\n", argv[0]);
+			printf("Running cpucount without options returns the number of\n");
+			printf("processors in the system.\n");
+			printf("-x1    - Number of processors in the system, plus 1.\n");
+			printf("-x2    - Number of processors in the system, doubled.\n");
+			return 0;
+		}
+	}
 
+	printf("%u\n", cpuCount);
 	return 0;
 }
