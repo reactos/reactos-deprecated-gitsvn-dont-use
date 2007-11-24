ca9e05ccd4700439ed5faabd8f1721abd0852f20
peterw <peterw@97493ccd-5924-5043-b1f5-66cb403b36ce>
2007-11-24 01:20:57 +0000

    - Fix a typo and add 'makex' to the MinGW prompt for convenience.
    git-svn-id: file:///srv/svn/reactos/trunk@30712 97493ccd-5924-5043-b1f5-66cb403b36ce

--- tools/RosBE/RosBE-Windows/Root/MinGW.cmd
+++ tools/RosBE/RosBE-Windows/Root/MinGW.cmd
@@ -1,13 +1,19 @@
 ::
 :: PROJECT:     RosBE - ReactOS Build Environment for Windows
 :: LICENSE:     GPL - See LICENSE.txt in the top level directory.
-:: FILE:        Root/RosBE.cmd
+:: FILE:        Root/MinGW.cmd
 :: PURPOSE:     This script provides/sets up a standard MinGW build environment.
 :: COPYRIGHT:   Copyright 2007 Daniel Reimer <reimer.daniel@freenet.de>
 ::                             Peter Ward <dralnix@gmail.com>
 ::
 ::
 @echo off
+
+if not "%1" == "" (
+    cls
+    echo Unknown parameter specified. Exiting.
+    goto :EOC
+)
 
 color 0A
 set PATH=%SystemRoot%\system32;%SystemRoot%
@@ -17,12 +23,6 @@ set _ROSBE_MODE=MinGW
 set _ROSBE_ORIGINALPATH=%PATH%
 set _ROSBE_MINGWPATH=%_ROSBE_BASEDIR%\4.1.3
 
-if not "%1" == "" (
-    cls
-    echo Unknown parameter specified. Exiting.
-    goto :EOC
-)
-
 echo.
 echo.
 call "%_ROSBE_BASEDIR%\rosbe-gcc-env.cmd"
@@ -30,5 +30,6 @@ echo.
 echo.
 
 doskey MAKE = "%_ROSBE_MINGWMAKE%" $*
+doskey MAKEX = "%_ROSBE_MINGWMAKE%" -j %NUMBER_OF_PROCESSORS% $*
 
 :EOC
