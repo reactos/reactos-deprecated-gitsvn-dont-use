ead60e512171f3be040862aa77a0f8a76850c192
frik85 <frik85@9874b476-3cf2-4131-8f6d-dcf341c5f767>
2008-02-20 20:16:40 +0000

    peoplemap lang update
    git-svn-id: file:///home/aleksey/repos/reactos_mirr/trunk@32434 9874b476-3cf2-4131-8f6d-dcf341c5f767

--- web/reactos.org/htdocs/peoplemap/lang/es.inc.php
+++ web/reactos.org/htdocs/peoplemap/lang/es.inc.php
@@ -1,4 +1,4 @@
-<?php
+ï»¿<?php
 /*
   PROJECT:    People Map of the ReactOS Website
   LICENSE:    GPL v2 or any later version
@@ -13,41 +13,41 @@
 
 	$peoplemap_langres["header"] = '<a href="http://www.reactos.org/">Home</a> &gt; ReactOS People Map';
 	$peoplemap_langres["title"] = "Mapa de Usuarios de ReactOS";
-	$peoplemap_langres["intro"] = "El siguiente mapa muestra la localización de los distintos usuarios y desarrolladores de ReactOS.";
+	$peoplemap_langres["intro"] = "El siguiente mapa muestra la localizaciÃ³n de los distintos usuarios y desarrolladores de ReactOS.";
 	
 	$peoplemap_langres["filter"] = "Filtrar por grupos de usuarios";
 	$peoplemap_langres["filter_intro"] = "Haz click en la casilla situada al lado de un grupo de usuarios para visualizarlos en el mapa o quitarlos.";
 	
-	$peoplemap_langres["add"] = "Buscar un usuario y añadirlo al mapa";
+	$peoplemap_langres["add"] = "Buscar un usuario y aÃ±adirlo al mapa";
 	$peoplemap_langres["add_username"] = "Nombre de usuario";
 	$peoplemap_langres["add_fullname"] = "Nombre completo";
 	
-	$peoplemap_langres["mylocation"] = "Mi ubicación en el mapa";
-	$peoplemap_langres["mylocation_login"] = "Debes iniciar sesión para situarte en el mapa.";
-	$peoplemap_langres["mylocation_login_page"] = "Página de Inicio de Sesión.";
-	$peoplemap_langres["mylocation_intro"] = "Aquí puedes situarte en el mapa. Tienes varias posibilidades para ello:";
-	$peoplemap_langres["mylocation_marker_intro"] = "Usa un marcador y arrastralo a tu ubicación sobre el mapa.";
+	$peoplemap_langres["mylocation"] = "Mi ubicaciÃ³n en el mapa";
+	$peoplemap_langres["mylocation_login"] = "Debes iniciar sesiÃ³n para situarte en el mapa.";
+	$peoplemap_langres["mylocation_login_page"] = "PÃ¡gina de Inicio de SesiÃ³n.";
+	$peoplemap_langres["mylocation_intro"] = "AquÃ­ puedes situarte en el mapa. Tienes varias posibilidades para ello:";
+	$peoplemap_langres["mylocation_marker_intro"] = "Usa un marcador y arrastralo a tu ubicaciÃ³n sobre el mapa.";
 	$peoplemap_langres["mylocation_marker_button"] = "Usa un marcador";
-	$peoplemap_langres["mylocation_marker_save_intro"] = "Haz click en este botón<br>para guardar tu ubicación.";
-	$peoplemap_langres["mylocation_marker_save_button"] = "Guardar mi ubicación";
+	$peoplemap_langres["mylocation_marker_save_intro"] = "Haz click en este botÃ³n<br>para guardar tu ubicaciÃ³n.";
+	$peoplemap_langres["mylocation_marker_save_button"] = "Guardar mi ubicaciÃ³n";
 	$peoplemap_langres["mylocation_marker_cancel"] = "Cancelar";
-	$peoplemap_langres["mylocation_coordinates_intro"] = "Introduce las coordenaddas en grados decimales:";
+	$peoplemap_langres["mylocation_coordinates_intro"] = "Introduce las coordenadas en grados decimales:";
 	$peoplemap_langres["mylocation_coordinates_button"] = "Fijar";
-	$peoplemap_langres["mylocation_coordinates_invalid"] = "¡Las coordenadas no son válidas!";
-	$peoplemap_langres["mylocation_delete_intro"] = "Con el siguiente botón, puedes eliminar tu posición en el mapa.";
-	$peoplemap_langres["mylocation_delete_button"] = "Eliminar mi ubicación";
+	$peoplemap_langres["mylocation_coordinates_invalid"] = "Â¡Las coordenadas no son vÃ¡lidas!";
+	$peoplemap_langres["mylocation_delete_intro"] = "Con el siguiente botÃ³n, puedes eliminar tu posiciÃ³n en el mapa.";
+	$peoplemap_langres["mylocation_delete_button"] = "Eliminar mi ubicaciÃ³n";
 	
 	$peoplemap_langres["latitude"] = "Latitud";
 	$peoplemap_langres["longitude"] = "Longitud";
 	$peoplemap_langres["removefrommap"] = "Eliminar al usuario del mapa";
 	$peoplemap_langres["minquerylength"] = "Introduce al menos 3 caracteres.";
-	$peoplemap_langres["nousers"] = "¡No se encontró ningún usuario con esos datos!";
-	$peoplemap_langres["activatejs"] = "¡Debes activar JavaScript para usar el Mapa de Usuarios!";
+	$peoplemap_langres["nousers"] = "Â¡No se encontrÃ³ ningÃºn usuario con esos datos!";
+	$peoplemap_langres["activatejs"] = "Â¡Debes activar JavaScript para usar el Mapa de Usuarios!";
 	$peoplemap_langres["unsupportedbrowser"] = "Tu navegador no soporta el Mapa de Usuarios.";
 	
 	$peoplemap_langres["count1"] = "Mostrando <strong>";
 	$peoplemap_langres["count2"] = " de ";
-	$peoplemap_langres["count3"] = "</strong> usuarios con datos de ubicación (de ";
+	$peoplemap_langres["count3"] = "</strong> usuarios con datos de ubicaciÃ³n (de ";
 	$peoplemap_langres["count4"] = " usuarios en total)";
 	$peoplemap_langres["icons"] = "Icono a utilizar:";
 ?>
