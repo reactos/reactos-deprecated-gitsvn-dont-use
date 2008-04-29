f013ac758708231949c10648d93c0ad5e153bf00
dreimer <dreimer@9874b476-3cf2-4131-8f6d-dcf341c5f767>
2008-04-29 18:41:31 +0000

    Start of a PowerShell Port of RosBE. Right now it does not do anything useful, because my DOSKEY Replacement function does not set the aliases global and I dont have any idea how to correct this right now.
    git-svn-id: file:///home/aleksey/repos/reactos_mirr/trunk@33192 9874b476-3cf2-4131-8f6d-dcf341c5f767

---
+++ tools/RosBE/RosBE-Windows/Powershell/Build.ps1
@@ -0,0 +1,175 @@
+#
+# PROJECT:     RosBE - ReactOS Build Environment for Windows
+# LICENSE:     GNU General Public License v2. (see LICENSE.txt)
+# FILE:        Root/Build.cmd
+# PURPOSE:     Perform the build of ReactOS.
+# COPYRIGHT:   Copyright 2007 Daniel Reimer <reimer.daniel@freenet.de>
+#                             Colin Finck <mail@colinfinck.de>
+#                             Peter Ward <dralnix@gmail.com>
+#
+#
+
+#
+# Check if config.template.rbuild is newer than config.rbuild, if it is then
+# abort the build and inform the user.
+#
+if exist .\config.rbuild (
+    "%_ROSBE_BASEDIR%\Tools\chknewer.exe" .\config.template.rbuild .\config.rbuild
+    if !errorlevel! == 1 (
+        echo.
+        echo *** config.template.rbuild is newer than config.rbuild ***
+        echo *** aborting build. Please check for changes and       ***
+        echo *** update your config.rbuild.                         ***
+        echo.
+        endlocal
+        goto :EOC
+    )
+)
+
+#
+# Check if strip or ccache are being used and set the appropriate options.
+#
+if defined _ROSBE_STRIP (
+    if %_ROSBE_STRIP% == 1 (
+        set ROS_LEAN_AND_MEAN=yes
+    ) else (
+        set ROS_LEAN_AND_MEAN=no
+    )
+)
+if defined _ROSBE_USECCACHE (
+    if %_ROSBE_USECCACHE% == 1 (
+        set CCACHE_DIR=%APPDATA%\RosBE\.ccache
+        set HOST_CC=ccache gcc
+        set HOST_CPP=ccache g++
+        set TARGET_CC=ccache gcc
+        set TARGET_CPP=ccache g++
+    ) else (
+        set HOST_CC=gcc
+        set HOST_CPP=g++
+        set TARGET_CC=gcc
+        set TARGET_CPP=g++
+    )
+)
+
+#
+# Check if the user has chosen to use a different object or output path
+# and set it accordingly.
+#
+if defined _ROSBE_OBJPATH (
+    if not exist "%_ROSBE_OBJPATH%\." (
+        echo ERROR: The path specified doesn't seem to exist.
+        goto :EOC
+    ) else (
+        set ROS_INTERMEDIATE=%_ROSBE_OBJPATH%
+    )
+)
+if defined _ROSBE_OUTPATH (
+    if not exist "%_ROSBE_OUTPATH%\." (
+        echo ERROR: The path specified doesn't seem to exist.
+        goto :EOC
+    ) else (
+        set ROS_OUTPUT=%_ROSBE_OUTPATH%
+        set ROS_TEMPORARY=%_ROSBE_OUTPATH%
+    )
+)
+
+#
+# Get the current date and time for use in in our build log's file name.
+#
+call "%_ROSBE_BASEDIR%\TimeDate.cmd"
+
+#
+# Check if writing logs is enabled, if so check if our log directory
+# exists, if it doesn't, create it.
+#
+if %_ROSBE_WRITELOG% == 1 (
+    if not exist "%_ROSBE_LOGDIR%\." (
+        mkdir "%_ROSBE_LOGDIR%" 1> NUL 2> NUL
+    )
+)
+
+#
+# Check if we are using -j or not.
+#
+if "%1" == "multi" (
+    if not "%2" == "" (
+        title 'makex %2' parallel build started: %TIMERAW%
+    ) else (
+        title 'makex' parallel build started: %TIMERAW%
+    )
+    call :BUILDMULTI %*
+) else (
+    if not "%1" == "" (
+        title 'make %1' build started: %TIMERAW%
+    ) else (
+        title 'make' build started: %TIMERAW%
+    )
+    call :BUILD %*
+)
+goto :EOC
+
+:BUILD
+    if %_ROSBE_SHOWTIME% == 1 (
+        if %_ROSBE_WRITELOG% == 1 (
+            "%_ROSBE_BASEDIR%\Tools\buildtime.exe" "%_ROSBE_MINGWMAKE%" %* 2>&1 | "%_ROSBE_BASEDIR%\Tools\tee.exe" "%_ROSBE_LOGDIR%\BuildLog-%_ROSBE_GCCVERSION%-%DATENAME%-%TIMENAME%.txt"
+        ) else (
+            "%_ROSBE_BASEDIR%\Tools\buildtime.exe" "%_ROSBE_MINGWMAKE%" %*
+        )
+    ) else (
+        if %_ROSBE_WRITELOG% == 1 (
+            "%_ROSBE_MINGWMAKE%" %* 2>&1 | "%_ROSBE_BASEDIR%\Tools\tee.exe" "%_ROSBE_LOGDIR%\BuildLog-%_ROSBE_GCCVERSION%-%DATENAME%-%TIMENAME%.txt"
+        ) else (
+            "%_ROSBE_MINGWMAKE%" %*
+        )
+    )
+goto :EOF
+
+:BUILDMULTI
+    #
+    # Get the number of CPUs in the system so we know how many jobs to execute.
+    # To modify the number used alter the options used with cpucount:
+    # No Option - Number of CPUs.
+    # -x1       - Number of CPUs, plus 1.
+    # -x2       - Number of CPUs, doubled.
+    # -a        - Determine the cpu count based on the inherited process affinity mask.
+    #
+    for /f "usebackq" %%i in (`"%_ROSBE_BASEDIR%\Tools\cpucount.exe" -x1`) do set CPUCOUNT=%%i
+
+    if %_ROSBE_SHOWTIME% == 1 (
+        if %_ROSBE_WRITELOG% == 1 (
+            "%_ROSBE_BASEDIR%\Tools\buildtime.exe" "%_ROSBE_MINGWMAKE%" -j %CPUCOUNT% %2 %3 %4 %5 %6 %7 %8 %9 2>&1 | "%_ROSBE_BASEDIR%\Tools\tee.exe" "%_ROSBE_LOGDIR%\BuildLog-%_ROSBE_GCCVERSION%-%DATENAME%-%TIMENAME%.txt"
+        ) else (
+            "%_ROSBE_BASEDIR%\Tools\buildtime.exe" "%_ROSBE_MINGWMAKE%" -j %CPUCOUNT% %2 %3 %4 %5 %6 %7 %8 %9
+        )
+    ) else (
+        if %_ROSBE_WRITELOG% == 1 (
+            "%_ROSBE_MINGWMAKE%" -j %CPUCOUNT% %2 %3 %4 %5 %6 %7 %8 %9 2>&1 | "%_ROSBE_BASEDIR%\Tools\tee.exe" "%_ROSBE_LOGDIR%\BuildLog-%_ROSBE_GCCVERSION%-%DATENAME%-%TIMENAME%.txt"
+        ) else (
+            "%_ROSBE_MINGWMAKE%" -j %CPUCOUNT% %2 %3 %4 %5 %6 %7 %8 %9
+        )
+    )
+goto :EOF
+
+:EOC
+#
+# Highlight the fact that building has ended.
+#
+"%_ROSBE_BASEDIR%\Tools\flash.exe"
+
+if defined _ROSBE_VERSION (
+    title ReactOS Build Environment %_ROSBE_VERSION%
+)
+
+#
+# Unload all used Vars.
+#
+set ROS_LEAN_AND_MEAN=
+set HOST_CC=
+set HOST_CPP=
+set TARGET_CC=
+set TARGET_CPP=
+set ROS_INTERMEDIATE=
+set ROS_OUTPUT=
+set ROS_TEMPORARY=
+set CPUCOUNT=
+set CCACHE_DIR=
---
+++ tools/RosBE/RosBE-Windows/Powershell/rosbe-gcc-env.ps1
@@ -0,0 +1,67 @@
+#
+# PROJECT:     RosBE - ReactOS Build Environment for Windows
+# LICENSE:     GNU General Public License v2. (see LICENSE.txt)
+# FILE:        Root/rosbe-gcc-env.cmd
+# PURPOSE:     Set up the GCC 4.x.x build environment.
+# COPYRIGHT:   Copyright 2007 Daniel Reimer <reimer.daniel@freenet.de>
+#                             Peter Ward <dralnix@gmail.com>
+#
+#
+
+#
+# Check if we are running within the RosBE, and if not
+# initialize GCC for the current directory.
+#
+if not defined _ROSBE_MINGWPATH (
+    set _ROSBE_MODE=MinGW
+    set _ROSBE_MINGWPATH=%CD%
+    set _ROSBE_ORIGINALPATH=%PATH%
+)
+
+#
+# Set up the GCC 4.x.x build environment.
+#
+set PATH=%_ROSBE_MINGWPATH%\bin;%_ROSBE_ORIGINALPATH%
+set _ROSBE_GCCVERSION=
+set _ROSBE_GCCTARGET=
+for /f "usebackq tokens=3" %%i in (`"gcc -v 2>&1 | find "gcc version""`) do set _ROSBE_GCCVERSION=%%i
+for /f "usebackq tokens=2" %%i in (`"gcc -v 2>&1 | find "Target""`) do set _ROSBE_GCCTARGET=%%i
+set PATH=%_ROSBE_MINGWPATH%\bin;%_ROSBE_MINGWPATH%\libexec\gcc\%_ROSBE_GCCTARGET%\%_ROSBE_GCCVERSION%;%_ROSBE_ORIGINALPATH%
+set _ROSBE_MINGWMAKE=%_ROSBE_MINGWPATH%\bin\mingw32-make.exe
+if "%_ROSBE_MODE%" == "MinGW" (
+    set C_INCLUDE_PATH=%_ROSBE_MINGWPATH%\include;%_ROSBE_MINGWPATH%\lib\gcc\%_ROSBE_GCCTARGET%\%_ROSBE_GCCVERSION%\include
+    set CPLUS_INCLUDE_PATH=%_ROSBE_MINGWPATH%\include;%_ROSBE_MINGWPATH%\include\c++\%_ROSBE_GCCVERSION%;%_ROSBE_MINGWPATH%\include\c++\%_ROSBE_GCCVERSION%\%_ROSBE_GCCTARGET%;%_ROSBE_MINGWPATH%\lib\gcc\%_ROSBE_GCCTARGET%\%_ROSBE_GCCVERSION%\include
+) else (
+    set HOST_CFLAGS=-I"%_ROSBE_MINGWPATH%\include" -I"%_ROSBE_MINGWPATH%\lib\gcc\%_ROSBE_GCCTARGET%\%_ROSBE_GCCVERSION%\include"
+    set HOST_CPPFLAGS=-I"%_ROSBE_MINGWPATH%\include" -I"%_ROSBE_MINGWPATH%\include\c++\%_ROSBE_GCCVERSION%" -I"%_ROSBE_MINGWPATH%\include\c++\%_ROSBE_GCCVERSION%\%_ROSBE_GCCTARGET%" -I"%_ROSBE_MINGWPATH%\lib\gcc\%_ROSBE_GCCTARGET%\%_ROSBE_GCCVERSION%\include"
+)
+set LIBRARY_PATH=%_ROSBE_MINGWPATH%\lib;%_ROSBE_MINGWPATH%\lib\gcc\%_ROSBE_GCCTARGET%\%_ROSBE_GCCVERSION%
+
+#
+# Display the current version of GCC, NASM, ld and make.
+#
+gcc -v 2>&1 | find "gcc version"
+echo gcc target^: %_ROSBE_GCCTARGET%
+ld -v
+if exist "%_ROSBE_MINGWPATH%\bin\nasm.exe" (
+    nasm -v
+) else (
+    if "%_ROSBE_MODE%" == "RosBE" (
+        echo ERROR: NASM is required to build ReactOS, none found in the current MinGW/GCC.
+    )
+)
+if exist "%_ROSBE_MINGWPATH%\bin\bison.exe" (
+    bison --version | find "GNU Bison"
+) else (
+    if "%_ROSBE_MODE%" == "RosBE" (
+        echo WARNING: Bison will soon be required to build ReactOS, none found in the current MinGW/GCC.
+    )
+)
+if exist "%_ROSBE_MINGWPATH%\bin\flex.exe" (
+    flex --version
+) else (
+    if "%_ROSBE_MODE%" == "RosBE" (
+        echo WARNING: Flex will soon be required to build ReactOS, none found in the current MinGW/GCC.
+    )
+)
+mingw32-make -v | find "GNU Make"
