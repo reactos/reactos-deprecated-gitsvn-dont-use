reactos/reactos-deprecated-gitsvn-dont-use

commit c4b61813bc7af67a8dfb0f5b4c758ce276dec16b

- make INewItem_Constructor compatible to be loaded as a shell extension - fix api definition of CDefFolderMenu_Create2 - disable loading of inewitem api (reenabled when default context menu api implementation is ready)
git-svn-id: file:///home/aleksey/repos/reactos_mirr/trunk@30366 9874b476-3cf2-4131-8f6d-dcf341c5f767

reactos/dll/win32/shell32/shv_bg_cmenu.c
@@ -226,7 +226,7 @@ static HRESULT WINAPI ISVBgCm_fnQueryContextMenu(
      * FIXME
      * load other shell extensions
      */
-    
+#if 0    
     if (SUCCEEDED(INewItem_Constructor(This->pSFParent, &IID_IContextMenu2, (LPVOID*)&icm)))
     {
         if (SUCCEEDED(IContextMenu_QueryContextMenu(icm, hMenu, 10, idCmdFirst, idCmdLast, uFlags)))
@@ -239,6 +239,7 @@ static HRESULT WINAPI ISVBgCm_fnQueryContextMenu(
             This->icm_new = NULL;
         }
     }
+#endif
 
     if (This->bDesktop)
     {

reactos/dll/win32/shell32/shv_item_new.c
@@ -55,7 +55,7 @@ typedef struct
     PSHELLNEW_ITEM s_SnHead;
 }INewMenuImpl;
 
-static const GUID CLSID_NewMenu = {0xD969A300, 0xE7FF, 0x11d0, {0xA9, 0x3B, 0x00, 0xA0, 0xC9, 0x0F, 0x27, 0x19} };
+const GUID CLSID_NewMenu = {0xD969A300, 0xE7FF, 0x11d0, {0xA9, 0x3B, 0x00, 0xA0, 0xC9, 0x0F, 0x27, 0x19} };
 static const IContextMenu2Vtbl cmvt;
 static WCHAR szNew[100];
 
@@ -791,7 +791,7 @@ static const IContextMenu2Vtbl cmvt =
 	INewItem_IContextMenu_fnHandleMenuMsg
 };
 
-HRESULT WINAPI INewItem_Constructor(LPSHELLFOLDER pSFParent, REFIID riid, LPVOID *ppv)
+HRESULT WINAPI INewItem_Constructor(IUnknown * pUnkOuter, REFIID riid, LPVOID *ppv)
 {
     INewMenuImpl * ow;
     HRESULT res;
@@ -820,8 +820,8 @@ HRESULT WINAPI INewItem_Constructor(LPSHELLFOLDER pSFParent, REFIID riid, LPVOID
     {
         IShellFolder_Release(cached_ow->pSFParent);
     }
-    cached_ow->pSFParent = pSFParent;
-    IShellFolder_AddRef(pSFParent);
+ //   cached_ow->pSFParent = pSFParent;
+ //   IShellFolder_AddRef(pSFParent);
 
     res = INewItem_fnQueryInterface( cached_ow, riid, ppv );
     return res;

reactos/dll/win32/shell32/shell32_main.h
@@ -82,7 +82,7 @@ LPENUMFORMATETC	IEnumFORMATETC_Constructor(UINT, const FORMATETC []);
 
 LPCLASSFACTORY	IClassFactory_Constructor(REFCLSID);
 IContextMenu2 *	ISvItemCm_Constructor(LPSHELLFOLDER pSFParent, LPCITEMIDLIST pidl, LPCITEMIDLIST *aPidls, UINT uItemCount);
-HRESULT WINAPI INewItem_Constructor(LPSHELLFOLDER pSFParent, REFIID riid, LPVOID *ppv);
+HRESULT WINAPI INewItem_Constructor(IUnknown * pUnkOuter, REFIID riif, LPVOID *ppv);
 IContextMenu2 * ISvStaticItemCm_Constructor(LPSHELLFOLDER pSFParent, LPCITEMIDLIST pidl, LPCITEMIDLIST *apidl, UINT cidl, HKEY hKey);
 IContextMenu2 *	ISvBgCm_Constructor(LPSHELLFOLDER pSFParent, BOOL bDesktop);
 LPSHELLVIEW	IShellView_Constructor(LPSHELLFOLDER);

reactos/dll/win32/shell32/shellole.c
@@ -78,6 +78,7 @@ static const struct {
     {&CLSID_Printers,       &ISF_Printers_Constructor},
 	{&CLSID_RecycleBin,     &RecycleBin_Constructor},
     {&CLSID_OpenWith,       &SHEOW_Constructor},
+    {&CLSID_NewMenu,        &INewItem_Constructor},
 	{NULL,NULL}
 };
 

reactos/dll/win32/shell32/shv_item_cmenu.c
@@ -1291,7 +1291,9 @@ HRESULT WINAPI SHCreateDefaultContextMenu(
  *
  */
 
-INT CDefFolderMenu_Create2(
+INT 
+WINAPI
+CDefFolderMenu_Create2(
 	LPCITEMIDLIST pidlFolder,
 	HWND hwnd,
 	UINT cidl,

commit 100b78266267c62418f03d8e188806c6329d8af7

Display again the 'New' context menu, which was broken in r30366 See issue #3183 for more details.
git-svn-id: file:///home/aleksey/repos/reactos_mirr/trunk@33592 9874b476-3cf2-4131-8f6d-dcf341c5f767

reactos/dll/win32/shell32/shv_bg_cmenu.c
@@ -170,6 +170,8 @@ static ULONG WINAPI ISVBgCm_fnRelease(IContextMenu2 *iface)
 * ISVBgCm_fnQueryContextMenu()
 */
 
+VOID INewItem_SetParent(LPSHELLFOLDER pSFParent); /* FIXME: remove that, shouldn't be needed */
+
 static HRESULT WINAPI ISVBgCm_fnQueryContextMenu(
 	IContextMenu2 *iface,
 	HMENU hMenu,
@@ -225,8 +227,7 @@ static HRESULT WINAPI ISVBgCm_fnQueryContextMenu(
      * FIXME
      * load other shell extensions
      */
-#if 0    
-    if (SUCCEEDED(INewItem_Constructor(This->pSFParent, &IID_IContextMenu2, (LPVOID*)&icm)))
+    if (SUCCEEDED(INewItem_Constructor(NULL, &IID_IContextMenu2, (LPVOID*)&icm)))
     {
         if (SUCCEEDED(IContextMenu_QueryContextMenu(icm, hMenu, 10, idCmdFirst, idCmdLast, uFlags)))
         {
@@ -238,7 +239,10 @@ static HRESULT WINAPI ISVBgCm_fnQueryContextMenu(
             This->icm_new = NULL;
         }
     }
-#endif
+
+    /* Prepare 'New item' shell extension */
+    /* FIXME: shouldn't be needed... */
+    INewItem_SetParent(This->pSFParent);
 
     if (This->bDesktop)
     {

reactos/dll/win32/shell32/shv_item_new.c
@@ -756,8 +756,8 @@ INewItem_IContextMenu_fnHandleMenuMsg(IContextMenu2 *iface,
 	                     LPARAM lParam)
 {
     INewMenuImpl *This = impl_from_IContextMenu(iface);
-    DRAWITEMSTRUCT * lpids = (DRAWITEMSTRUCT*) lParam;
-    MEASUREITEMSTRUCT *lpmis = (MEASUREITEMSTRUCT*) lParam;
+    //DRAWITEMSTRUCT * lpids = (DRAWITEMSTRUCT*) lParam;
+    //MEASUREITEMSTRUCT *lpmis = (MEASUREITEMSTRUCT*) lParam;
 
 	TRACE("INewItem_IContextMenu_fnHandleMenuMsg (%p)->(msg=%x wp=%lx lp=%lx)\n",This, uMsg, wParam, lParam);
 
@@ -789,11 +789,11 @@ static const IContextMenu2Vtbl cmvt =
 	INewItem_IContextMenu_fnHandleMenuMsg
 };
 
+static INewMenuImpl *cached_ow;
 HRESULT WINAPI INewItem_Constructor(IUnknown * pUnkOuter, REFIID riid, LPVOID *ppv)
 {
     INewMenuImpl * ow;
     HRESULT res;
-    static INewMenuImpl *cached_ow;
 
     if (!cached_ow)
     {
@@ -814,13 +814,17 @@ HRESULT WINAPI INewItem_Constructor(IUnknown * pUnkOuter, REFIID riid, LPVOID *p
     }
 
     TRACE("(%p)->()\n",cached_ow);
+
+    res = INewItem_fnQueryInterface( cached_ow, riid, ppv );
+    return res;
+}
+
+VOID INewItem_SetParent(LPSHELLFOLDER pSFParent)
+{
     if (cached_ow->pSFParent)
     {
         IShellFolder_Release(cached_ow->pSFParent);
     }
- //   cached_ow->pSFParent = pSFParent;
- //   IShellFolder_AddRef(pSFParent);
-
-    res = INewItem_fnQueryInterface( cached_ow, riid, ppv );
-    return res;
+    cached_ow->pSFParent = pSFParent;
+    IShellFolder_AddRef(pSFParent);
 }