77880c1ab3b12e78196dadc43e84ef2d63454999
cfinck <cfinck@97493ccd-5924-5043-b1f5-66cb403b36ce>
2008-06-11 20:02:19 +0000

    - Fix/unhack/simplify/rewrite the RosCMS authentication module for phpBB.   This should finally fix all Login/Logout problems people were experiencing. - Don't do any RosCMS-related stuff in login_box(), this is all done in the Authentication module now. - Disable the phpBB autologin, it interferes with the RosCMS Login.
    git-svn-id: file:///srv/svn/reactos/trunk@33936 97493ccd-5924-5043-b1f5-66cb403b36ce

--- web/reactos.org/htdocs/forum/includes/auth/auth_roscms.php
+++ web/reactos.org/htdocs/forum/includes/auth/auth_roscms.php
@@ -12,52 +12,23 @@ if (!defined('IN_PHPBB'))
 	exit;
 }
 
+require_once("$phpbb_root_path/../roscms/inc/subsys_login.php");
+
 /**
 * Login function
 */
 define(ROSCMS_DB_NAME, "roscms");
 
-function login_roscms(&$userid, &$password)
+function login_roscms(&$username, &$password)
 {
 	global $db, $config;
 	
-	// We only check the User ID here.
-	if (!$userid)
-	{
-		return array(
-			'status'	=> LOGIN_ERROR_USERNAME,
-			'error_msg'	=> 'LOGIN_ERROR_USERNAME',
-			'user_row'	=> array('user_id' => ANONYMOUS),
-		);
-	}
-
-	// Get the phpBB ID of the user belonging to the "roscmsusrkey" cookie
-	$sql = "SELECT m.map_subsys_userid " .
-	       "FROM " . ROSCMS_DB_NAME . ".user_sessions s, " . ROSCMS_DB_NAME . ".users u, " . ROSCMS_DB_NAME . ".subsys_mappings m " .
-	       "WHERE s.usersession_id = '" . $db->sql_escape($_COOKIE["roscmsusrkey"]) . "' AND (s.usersession_expires IS NULL OR NOW() <= s.usersession_expires) " .
-	       "AND u.user_id = s.usersession_user_id AND (u.user_setting_ipaddress = 'false' OR s.usersession_ipaddress = '" . $db->sql_escape($_SERVER["REMOTE_ADDR"]) . "') " .
-	       "AND (u.user_setting_browseragent = 'false' OR s.usersession_browseragent = '" . $db->sql_escape($_SERVER["HTTP_USER_AGENT"]) . "') " .
-	       "AND m.map_roscms_userid = s.usersession_user_id " .
-	       "AND m.map_subsys_name = 'phpbb'";
-	$result = $db->sql_query($sql);
-	$row = $db->sql_fetchrow($result);
-	$db->sql_freeresult($result);
+	// We ignore both username and password here and retrieve the login data on our own using roscms_subsys_login
+	// This will either retrieve the phpbb user ID of the user currently logged in or redirect us to the RosCMS login page.
+	$userid = (int)roscms_subsys_login("phpbb", ROSCMS_LOGIN_REQUIRED, "/forum");
 	
-	// Verify this ID against $userid
-	if(!$row || $row["map_subsys_userid"] != $userid)
-	{
-		return array(
-			'status'	=> LOGIN_ERROR_USERNAME,
-			'error_msg'	=> 'LOGIN_ERROR_USERNAME',
-			'user_row'	=> array('user_id' => ANONYMOUS),
-		);
-	}
-	
-	// Now get the user information based on this ID
-	$sql = "SELECT user_id, username, user_password, user_passchg, user_pass_convert, user_email, user_type, user_login_attempts " .
-	       "FROM " . USERS_TABLE . " " .
-	       "WHERE user_id = " . (int)$userid;
-	
+	// Now get the user row based on this ID
+	$sql = "SELECT * FROM " . USERS_TABLE . " WHERE user_id = $userid";
 	$result = $db->sql_query($sql);
 	$row = $db->sql_fetchrow($result);
 	$db->sql_freeresult($result);
@@ -89,29 +60,38 @@ function login_roscms(&$userid, &$password)
 	);
 }
 
+/* This function is called, when a session cookie already exists and we try to verify if it's valid. */
 function validate_session_roscms(&$user)
+{
+	// Check if our current RosCMS login is (still) valid, check the session expiration time and perform session cleanups.
+	$valid_login = (roscms_subsys_login("phpbb", ROSCMS_LOGIN_OPTIONAL, "") != 0);
+	
+	// If we have a valid login, but the phpBB user ID is still ANONYMOUS, the user was logged in to RosCMS, but not yet to phpBB.
+	// So do that now.
+	if($valid_login && $user["user_id"] == ANONYMOUS)
+		login_box();
+	
+	return $valid_login;
+}
+
+/* This function is called, when no phpBB session exists and we're in the process of creating the session cookie. */
+function autologin_roscms()
 {
 	global $db;
 	
-	if($_COOKIE["roscmsusrkey"])
-	{
-		if($user["user_id"] == ANONYMOUS)
-		{
-			// The user is logged in in RosCMS, but not yet in phpBB. Do that now
-			login_box();
-		}
-		else
-		{
-			// Update the session expiration time
-			$sql = "UPDATE " . ROSCMS_DB_NAME . ".user_sessions " .
-			       "SET usersession_expires = DATE_ADD(NOW(), INTERVAL 30 MINUTE) " .
-			       "WHERE usersession_id = '" . $db->sql_escape($_COOKIE["roscmsusrkey"]) . "' " .
-			       "AND usersession_expires IS NOT NULL";
-			$db->sql_query($sql);
-		}
-	}
+	// Get the User ID of the logged in user (if any), check the session expiration time and perform session cleanups.
+	$userid = (int)roscms_subsys_login("phpbb", ROSCMS_LOGIN_OPTIONAL, "");
 	
-	return true;
+	if($userid)
+	{
+		// Return the phpBB user row if a user is logged in.
+		$sql = "SELECT * FROM " . USERS_TABLE . " WHERE user_id = $userid";
+		$result = $db->sql_query($sql);
+		$row = $db->sql_fetchrow($result);
+		$db->sql_freeresult($result);
+		
+		return $row;
+	}
 }
 
 ?>
