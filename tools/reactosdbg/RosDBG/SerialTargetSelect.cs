8e6df9c34005aad14eafa01e6c57abf0bd437653
arty <arty@9874b476-3cf2-4131-8f6d-dcf341c5f767>
2008-08-09 12:40:32 +0000

    Also forgottent from Gregor Schneider's patch (sorry)
    git-svn-id: file:///home/aleksey/repos/reactos_mirr/trunk@35223 9874b476-3cf2-4131-8f6d-dcf341c5f767

---
+++ tools/reactosdbg/RosDBG/SerialTargetSelect.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace RosDBG
+{
+    public partial class SerialTargetSelect : Form
+    {
+        public String Port
+        {
+            get { return cPort.Text; }
+            set { Port = value; }
+        }
+
+        public int Baudrate
+        {
+            get { return Convert.ToInt32(cBaud.Text); }
+            set { Baudrate = value; }
+        }
+
+        public SerialTargetSelect()
+        {
+            InitializeComponent();
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                cPort.Items.Add(s);
+            }
+            cPort.SelectedIndex = 0;
+            cBaud.SelectedIndex = 0;
+        }
+
+        private void bOK_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void bCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
