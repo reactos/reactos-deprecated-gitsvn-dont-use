c22a6676fecb050f2e8848d87592e4c1f8c4a934
gedmurphy <gedmurphy@97493ccd-5924-5043-b1f5-66cb403b36ce>
2008-08-11 14:52:19 +0000

    Add missing files.
    git-svn-id: file:///srv/svn/reactos/trunk@35276 97493ccd-5924-5043-b1f5-66cb403b36ce

---
+++ tools/reactosdbg/RosDBG/PipeTargetSelect.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RosDBG
+{
+    public partial class PipeTargetSelect : Form
+    {
+        public string PipeName
+        {
+            get { return PipeNameTextBox.Text; }
+            set { PipeNameTextBox.Text = value; }
+        }
+
+        public PipeTargetSelect()
+        {
+            InitializeComponent();
+        }
+
+        private void bOK_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void bCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = (RadioButton)sender;
+
+            PipeNameTextBox.Enabled = rb.Checked;
+        }
+    }
+}
