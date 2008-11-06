633e74bcd83af35a52e909bef1cff2fe923451d0
dreimer <dreimer@97493ccd-5924-5043-b1f5-66cb403b36ce>
2008-11-06 22:09:47 +0000

    Hello to three new params on Powershell. Still six are left. Synched the others to the latest changes in the cmd files.
    git-svn-id: file:///srv/svn/reactos/trunk@37233 97493ccd-5924-5043-b1f5-66cb403b36ce

---
+++ tools/RosBE/RosBE-Windows/Powershell/chdefdir.ps1
@@ -0,0 +1,46 @@
+#
+# PROJECT:     RosBE - ReactOS Build Environment for Windows
+# LICENSE:     GNU General Public License v2. (see LICENSE.txt)
+# FILE:        Root/chdefdir.cmd
+# PURPOSE:     Tool to change the current working ReactOS source directory
+#              in RosBE.
+# COPYRIGHT:   Copyright 2007 Daniel Reimer <reimer.daniel@freenet.de>
+#                             Peter Ward <dralnix@gmail.com>
+#
+#
+
+$host.ui.RawUI.WindowTitle = "Change the current working ReactOS source directory..."
+
+#
+# Parse the command line arguments.
+#
+if ($args[0] -eq $null) {
+    #
+    # If Parameters were set, parse them, if not, ask the user to add them.
+    #
+    $_1 = Read-Host "Please enter a ReactOS source directory, or 'previous': "
+    if ($_1 -eq $null) {
+        "ERROR: You must enter a ReactOS source directory, or 'previous'."
+    }
+} else {
+    $_1 = $args
+}
+if ($_1 -eq "previous") {
+    popd
+} else {
+    if (!(Test-Path "$_1\.")) {
+        "ERROR: The path specified doesn't seem to exist."
+        
+    }
+    pushd "$_1"
+}
+$global:_ROSBE_ROSSOURCEDIR = "$pwd"
+
+if ($_ROSBE_VERSION -ne $null) {
+    $host.ui.RawUI.WindowTitle = "ReactOS Build Environment $_ROSBE_VERSION"
+}
+
+#
+# Unload all used Vars.
+#
+$_1 = $null
---
+++ tools/RosBE/RosBE-Windows/Powershell/options.ps1
@@ -0,0 +1,28 @@
+#
+# PROJECT:     RosBE - ReactOS Build Environment for Windows
+# LICENSE:     GNU General Public License v2. (see LICENSE.txt)
+# FILE:        Root/options.cmd
+# PURPOSE:     Starts options.exe and restarts RosBE afterwards.
+# COPYRIGHT:   Copyright 2007 Daniel Reimer <reimer.daniel@freenet.de>
+#
+#
+
+$host.ui.RawUI.WindowTitle = "Options"
+
+#
+# Run options.exe
+#
+if (Test-Path "$_ROSBE_BASEDIR\Tools\options.exe") {
+    pushd "$_ROSBE_BASEDIR"
+    & "$_ROSBE_BASEDIR\Tools\options.exe"
+    popd
+    if (Test-Path "$ENV:APPDATA\RosBE\rosbe-options.cmd") {
+        & "$ENV:APPDATA\RosBE\rosbe-options.cmd"
+    }
+} else {
+    "ERROR: options.exe was not found."
+}
+
+if ($_ROSBE_VERSION -ne $null) {
+    $host.ui.RawUI.WindowTitle = "ReactOS Build Environment $_ROSBE_VERSION"
+}
