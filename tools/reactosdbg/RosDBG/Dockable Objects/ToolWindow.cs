4945ded20c164544efe6891dfdc0065c577cdadf
cwittich <cwittich@97493ccd-5924-5043-b1f5-66cb403b36ce>
2009-05-28 13:03:35 +0000

    add missing files
    git-svn-id: file:///srv/svn/reactos/trunk@41179 97493ccd-5924-5043-b1f5-66cb403b36ce

---
+++ tools/reactosdbg/RosDBG/Dockable Objects/ToolWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace RosDBG
+{
+    public partial class ToolWindow : DockContent
+    {
+
+        public enum Commands {Copy, Cut, Paste, Save, SaveAs, Print};
+
+        public ToolWindow()
+        {
+            InitializeComponent();
+        }
+
+        public virtual void SaveAs(string FileName)
+        {
+            MessageBox.Show("Not implemented!");
+        }
+
+        public virtual void Save(string FileName)
+        {
+            SaveAs(FileName);
+        }
+
+        public virtual string GetDocumentName()
+        {
+            return "";
+        }
+
+        public virtual bool IsCmdEnabled(Commands Cmd)
+        {
+            return false;
+        }
+
+    }
+}
