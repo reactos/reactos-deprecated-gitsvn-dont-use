reactos/reactos-deprecated-gitsvn-dont-use

commit 266ad443b6abfb97f72c12ae3529bc9e8f1297e7

[SCSIPORT] - Don't stop enumerating functions of a PCI device if current function returned invalid vendor id. Also add a check for returned data size. This fixes detection of some storage controllers, e.g. Intel ICH8, ICH9, ICH10 chipsets. Thanks to Alter (UniATA author) for finding the problem and providing solution. See issue #7147 for more details.
git-svn-id: file:///srv/svn/reactos/trunk@56863 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/drivers/storage/scsiport/scsiport.c
@@ -2350,10 +2350,13 @@ SpiGetPciConfigData(IN PDRIVER_OBJECT DriverObject,
             if (DataSize == 0)
                 return FALSE;
 
-            /* If result is PCI_INVALID_VENDORID, then this device has no more
-               "Functions" */
-            if (PciConfig.VendorID == PCI_INVALID_VENDORID)
-                break;
+            /* Check if result is PCI_INVALID_VENDORID or too small */
+            if ((DataSize < (ULONG)PCI_COMMON_HDR_LENGTH) ||
+                (PciConfig.VendorID == PCI_INVALID_VENDORID))
+            {
+                /* Continue to try the next function */
+                continue;
+            }
 
             sprintf (VendorIdString, "%04hx", PciConfig.VendorID);
             sprintf (DeviceIdString, "%04hx", PciConfig.DeviceID);

commit 0e25cc9d116c3f2deb154d0816525f3fcfdc86c7

[SCSIPORT] - Fix a regression from r56863. New logic is: the requested data size is sizeof(ULONG), so if the result read is less than that - continue to the next slot. Thanks to SomeGuy for pointing out the problem. See issue #7147 for more details.
git-svn-id: file:///srv/svn/reactos/trunk@56907 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/drivers/storage/scsiport/scsiport.c
@@ -2351,7 +2351,7 @@ SpiGetPciConfigData(IN PDRIVER_OBJECT DriverObject,
                 return FALSE;
 
             /* Check if result is PCI_INVALID_VENDORID or too small */
-            if ((DataSize < (ULONG)PCI_COMMON_HDR_LENGTH) ||
+            if ((DataSize < sizeof(ULONG)) ||
                 (PciConfig.VendorID == PCI_INVALID_VENDORID))
             {
                 /* Continue to try the next function */