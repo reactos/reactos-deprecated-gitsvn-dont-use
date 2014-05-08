reactos/reactos-deprecated-gitsvn-dont-use

commit 67ee7d764b8d8c65f41274a0d34ef24d3f3972a0

[User32] - Fixed more wine tests. WM_UNINITMENUPOPUP is called before destroying pop up menu window.
git-svn-id: file:///srv/svn/reactos/trunk@63183 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/win32ss/user/user32/windows/menu.c
@@ -2944,13 +2944,18 @@ MenuHideSubPopups(HWND WndOwner, PROSMENUINFO MenuInfo,
       {
           MenuHideSubPopups(WndOwner, &SubMenuInfo, FALSE, wFlags);
           MenuSelectItem(WndOwner, &SubMenuInfo, NO_SELECTED_ITEM, SendMenuSelect, NULL);
-          DestroyWindow(SubMenuInfo.Wnd);
-          SubMenuInfo.Wnd = NULL;
-          MenuSetRosMenuInfo(&SubMenuInfo);
 
           if (!(wFlags & TPM_NONOTIFY))
              SendMessageW( WndOwner, WM_UNINITMENUPOPUP, (WPARAM)ItemInfo.hSubMenu,
                                  MAKELPARAM(0, IS_SYSTEM_MENU(&SubMenuInfo)) );
+          ////
+          // Call WM_UNINITMENUPOPUP FIRST before destroy!!
+          // Fixes todo_wine User32 test menu.c line 2233 GetMenuBarInfo callback....
+          //
+          DestroyWindow(SubMenuInfo.Wnd);
+          SubMenuInfo.Wnd = NULL;
+          MenuSetRosMenuInfo(&SubMenuInfo);
+          ////
       }
   }
 }

commit 081b7252eaf7dafebcec36d43266330cc3a42b09

[User32] - Fixed regression from r63183. See CORE-7967.
git-svn-id: file:///srv/svn/reactos/trunk@63186 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/win32ss/user/user32/windows/menu.c
@@ -2945,6 +2945,8 @@ MenuHideSubPopups(HWND WndOwner, PROSMENUINFO MenuInfo,
           MenuHideSubPopups(WndOwner, &SubMenuInfo, FALSE, wFlags);
           MenuSelectItem(WndOwner, &SubMenuInfo, NO_SELECTED_ITEM, SendMenuSelect, NULL);
 
+          DestroyWindow(SubMenuInfo.Wnd);
+          /* Native returns handle to destroyed window */
           if (!(wFlags & TPM_NONOTIFY))
              SendMessageW( WndOwner, WM_UNINITMENUPOPUP, (WPARAM)ItemInfo.hSubMenu,
                                  MAKELPARAM(0, IS_SYSTEM_MENU(&SubMenuInfo)) );
@@ -2952,7 +2954,6 @@ MenuHideSubPopups(HWND WndOwner, PROSMENUINFO MenuInfo,
           // Call WM_UNINITMENUPOPUP FIRST before destroy!!
           // Fixes todo_wine User32 test menu.c line 2233 GetMenuBarInfo callback....
           //
-          DestroyWindow(SubMenuInfo.Wnd);
           SubMenuInfo.Wnd = NULL;
           MenuSetRosMenuInfo(&SubMenuInfo);
           ////
@@ -3966,6 +3967,7 @@ static INT FASTCALL MenuTrackMenu(HMENU hmenu, UINT wFlags, INT x, INT y,
                     IntNotifyWinEvent(EVENT_SYSTEM_MENUPOPUPEND, MenuInfo.Wnd, OBJID_CLIENT, CHILDID_SELF, 0);
                     DestroyWindow(MenuInfo.Wnd);
                     MenuInfo.Wnd = NULL;
+                    MenuSetRosMenuInfo(&MenuInfo);
 
                     if (!(wFlags & TPM_NONOTIFY))
                       SendMessageW( mt.OwnerWnd, WM_UNINITMENUPOPUP, (WPARAM)mt.TopMenu,