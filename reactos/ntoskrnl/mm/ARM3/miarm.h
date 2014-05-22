reactos/reactos-deprecated-gitsvn-dont-use

commit 3191b43218830b82f6a45861e9676dc5b703e12b

[NTOSKRNL] - Remove the BoundaryAddressMultiple parameter from MmCreateMemoryArea (wasn't used) and give it instead a Granularity parameter - Use the Granularity parameter in MmMapViewOfSegment to make sure that full sections are allocated on a MM_ALLOCATION_GRANULARITY aligned address. - Check for overflow and unaligned image base in MmMapViewOfSection when mapping image sections - Return proper status code on failure
git-svn-id: file:///srv/svn/reactos/trunk@61108 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/ntoskrnl/mm/marea.c
@@ -984,10 +984,8 @@ MmCreateMemoryArea(PMMSUPPORT AddressSpace,
                    PMEMORY_AREA *Result,
                    BOOLEAN FixedAddress,
                    ULONG AllocationFlags,
-                   PHYSICAL_ADDRESS BoundaryAddressMultiple)
+                   ULONG Granularity)
 {
-   PVOID EndAddress;
-   ULONG Granularity;
    ULONG_PTR tmpLength;
    PMEMORY_AREA MemoryArea;
 
@@ -997,7 +995,6 @@ MmCreateMemoryArea(PMMSUPPORT AddressSpace,
           Type, BaseAddress, *BaseAddress, Length, AllocationFlags,
           FixedAddress, Result);
 
-   Granularity = PAGE_SIZE;
    if ((*BaseAddress) == 0 && !FixedAddress)
    {
       tmpLength = (ULONG_PTR)MM_ROUND_UP(Length, Granularity);
@@ -1028,12 +1025,6 @@ MmCreateMemoryArea(PMMSUPPORT AddressSpace,
       {
          DPRINT("Memory area for user mode address space exceeds MmSystemRangeStart\n");
          return STATUS_ACCESS_VIOLATION;
-      }
-
-      if (BoundaryAddressMultiple.QuadPart != 0)
-      {
-         EndAddress = ((char*)(*BaseAddress)) + tmpLength-1;
-         ASSERT(((ULONG_PTR)*BaseAddress/BoundaryAddressMultiple.QuadPart) == ((DWORD_PTR)EndAddress/BoundaryAddressMultiple.QuadPart));
       }
 
       if (MmLocateMemoryAreaByRegion(AddressSpace,

reactos/ntoskrnl/cache/section/data.c
@@ -627,9 +627,6 @@ _MiMapViewOfSegment(PMMSUPPORT AddressSpace,
 {
     PMEMORY_AREA MArea;
     NTSTATUS Status;
-    PHYSICAL_ADDRESS BoundaryAddressMultiple;
-
-    BoundaryAddressMultiple.QuadPart = 0;
 
     Status = MmCreateMemoryArea(AddressSpace,
                                 MEMORY_AREA_CACHE,
@@ -639,7 +636,7 @@ _MiMapViewOfSegment(PMMSUPPORT AddressSpace,
                                 &MArea,
                                 FALSE,
                                 AllocationType,
-                                BoundaryAddressMultiple);
+                                PAGE_SIZE);
 
     if (!NT_SUCCESS(Status))
     {

reactos/ntoskrnl/cc/view.c
@@ -611,13 +611,11 @@ CcRosCreateCacheSegment (
 #ifdef CACHE_BITMAP
     ULONG StartingOffset;
 #endif
-    PHYSICAL_ADDRESS BoundaryAddressMultiple;
 
     ASSERT(Bcb);
 
     DPRINT("CcRosCreateCacheSegment()\n");
 
-    BoundaryAddressMultiple.QuadPart = 0;
     if (FileOffset >= Bcb->FileSize.u.LowPart)
     {
         CacheSeg = NULL;
@@ -742,7 +740,7 @@ CcRosCreateCacheSegment (
                                 (PMEMORY_AREA*)&current->MemoryArea,
                                 FALSE,
                                 0,
-                                BoundaryAddressMultiple);
+                                PAGE_SIZE);
     MmUnlockAddressSpace(MmGetKernelAddressSpace());
     if (!NT_SUCCESS(Status))
     {
@@ -1370,12 +1368,10 @@ CcInitView (
 #ifdef CACHE_BITMAP
     PMEMORY_AREA marea;
     PVOID Buffer;
-    PHYSICAL_ADDRESS BoundaryAddressMultiple;
 #endif
 
     DPRINT("CcInitView()\n");
 #ifdef CACHE_BITMAP
-    BoundaryAddressMultiple.QuadPart = 0;
     CiCacheSegMappingRegionHint = 0;
     CiCacheSegMappingRegionBase = NULL;
 
@@ -1389,7 +1385,7 @@ CcInitView (
                                 &marea,
                                 FALSE,
                                 0,
-                                BoundaryAddressMultiple);
+                                PAGE_SIZE);
     MmUnlockAddressSpace(MmGetKernelAddressSpace());
     if (!NT_SUCCESS(Status))
     {

reactos/ntoskrnl/include/internal/mm.h
@@ -512,7 +512,7 @@ MmCreateMemoryArea(
     PMEMORY_AREA *Result,
     BOOLEAN FixedAddress,
     ULONG AllocationFlags,
-    PHYSICAL_ADDRESS BoundaryAddressMultiple OPTIONAL
+    ULONG AllocationGranularity
 );
 
 PMEMORY_AREA

reactos/ntoskrnl/mm/ARM3/procsup.c
@@ -30,9 +30,7 @@ MiRosTakeOverSharedUserPage(IN PEPROCESS Process)
 {
     NTSTATUS Status;
     PMEMORY_AREA MemoryArea;
-    PHYSICAL_ADDRESS BoundaryAddressMultiple;
     PVOID AllocatedBase = (PVOID)MM_SHARED_USER_DATA_VA;
-    BoundaryAddressMultiple.QuadPart = 0;
 
     Status = MmCreateMemoryArea(&Process->Vm,
                                 MEMORY_AREA_OWNED_BY_ARM3,
@@ -42,7 +40,7 @@ MiRosTakeOverSharedUserPage(IN PEPROCESS Process)
                                 &MemoryArea,
                                 TRUE,
                                 0,
-                                BoundaryAddressMultiple);
+                                PAGE_SIZE);
     ASSERT(NT_SUCCESS(Status));
 }
 

reactos/ntoskrnl/mm/ARM3/vadnode.c
@@ -24,19 +24,19 @@
 
 CHAR MmReadWrite[32] =
 {
-    MM_NO_ACCESS_ALLOWED, MM_READ_ONLY_ALLOWED, MM_READ_ONLY_ALLOWED, 
+    MM_NO_ACCESS_ALLOWED, MM_READ_ONLY_ALLOWED, MM_READ_ONLY_ALLOWED,
     MM_READ_ONLY_ALLOWED, MM_READ_WRITE_ALLOWED, MM_READ_WRITE_ALLOWED,
     MM_READ_WRITE_ALLOWED, MM_READ_WRITE_ALLOWED,
 
-    MM_NO_ACCESS_ALLOWED, MM_READ_ONLY_ALLOWED, MM_READ_ONLY_ALLOWED, 
+    MM_NO_ACCESS_ALLOWED, MM_READ_ONLY_ALLOWED, MM_READ_ONLY_ALLOWED,
     MM_READ_ONLY_ALLOWED, MM_READ_WRITE_ALLOWED, MM_READ_WRITE_ALLOWED,
     MM_READ_WRITE_ALLOWED, MM_READ_WRITE_ALLOWED,
 
-    MM_NO_ACCESS_ALLOWED, MM_READ_ONLY_ALLOWED, MM_READ_ONLY_ALLOWED, 
+    MM_NO_ACCESS_ALLOWED, MM_READ_ONLY_ALLOWED, MM_READ_ONLY_ALLOWED,
     MM_READ_ONLY_ALLOWED, MM_READ_WRITE_ALLOWED, MM_READ_WRITE_ALLOWED,
     MM_READ_WRITE_ALLOWED, MM_READ_WRITE_ALLOWED,
 
-    MM_NO_ACCESS_ALLOWED, MM_READ_ONLY_ALLOWED, MM_READ_ONLY_ALLOWED, 
+    MM_NO_ACCESS_ALLOWED, MM_READ_ONLY_ALLOWED, MM_READ_ONLY_ALLOWED,
     MM_READ_ONLY_ALLOWED, MM_READ_WRITE_ALLOWED, MM_READ_WRITE_ALLOWED,
     MM_READ_WRITE_ALLOWED, MM_READ_WRITE_ALLOWED,
 };
@@ -129,11 +129,10 @@ MiInsertNode(IN PMM_AVL_TABLE Table,
     {
         NTSTATUS Status;
         PMEMORY_AREA MemoryArea;
-        PHYSICAL_ADDRESS BoundaryAddressMultiple;
         SIZE_T Size;
         PEPROCESS Process = CONTAINING_RECORD(Table, EPROCESS, VadRoot);
         PVOID AllocatedBase = (PVOID)(Vad->StartingVpn << PAGE_SHIFT);
-        BoundaryAddressMultiple.QuadPart = 0;
+
         Size = ((Vad->EndingVpn + 1) - Vad->StartingVpn) << PAGE_SHIFT;
         Status = MmCreateMemoryArea(&Process->Vm,
                                     MEMORY_AREA_OWNED_BY_ARM3,
@@ -143,7 +142,7 @@ MiInsertNode(IN PMM_AVL_TABLE Table,
                                     &MemoryArea,
                                     TRUE,
                                     0,
-                                    BoundaryAddressMultiple);
+                                    PAGE_SIZE);
         ASSERT(NT_SUCCESS(Status));
 
         /* Check if this is VM VAD */
@@ -635,7 +634,7 @@ MiCheckSecuredVad(IN PMMVAD Vad,
         /* This is allowed */
         ProtectionMask = 0;
     }
-    
+
     /* ARM3 doesn't support this yet */
     ASSERT(Vad->u2.VadFlags2.MultipleSecured == 0);
 
@@ -655,7 +654,7 @@ MiCheckSecuredVad(IN PMMVAD Vad,
 
             /* ARM3 doesn't have read-only VADs yet */
             ASSERT(Vad->u2.VadFlags2.ReadOnly == 0);
-            
+
             /* Check if read-write protections are allowed */
             if (MmReadWrite[ProtectionMask] < MM_READ_WRITE_ALLOWED)
             {

reactos/ntoskrnl/mm/arm/stubs.c
@@ -779,14 +779,12 @@ NTAPI
 MiInitPageDirectoryMap(VOID)
 {
     MEMORY_AREA* MemoryArea = NULL;
-    PHYSICAL_ADDRESS BoundaryAddressMultiple;
     PVOID BaseAddress;
     NTSTATUS Status;
 
     //
     // Create memory area for the PTE area
     //
-    BoundaryAddressMultiple.QuadPart = 0;
     BaseAddress = (PVOID)PTE_BASE;
     Status = MmCreateMemoryArea(MmGetKernelAddressSpace(),
                                 MEMORY_AREA_OWNED_BY_ARM3,
@@ -796,7 +794,7 @@ MiInitPageDirectoryMap(VOID)
                                 &MemoryArea,
                                 TRUE,
                                 0,
-                                BoundaryAddressMultiple);
+                                PAGE_SIZE);
     ASSERT(NT_SUCCESS(Status));
 
     //
@@ -811,7 +809,7 @@ MiInitPageDirectoryMap(VOID)
                                 &MemoryArea,
                                 TRUE,
                                 0,
-                                BoundaryAddressMultiple);
+                                PAGE_SIZE);
     ASSERT(NT_SUCCESS(Status));
 
     //
@@ -826,7 +824,7 @@ MiInitPageDirectoryMap(VOID)
                                 &MemoryArea,
                                 TRUE,
                                 0,
-                                BoundaryAddressMultiple);
+                                PAGE_SIZE);
     ASSERT(NT_SUCCESS(Status));
 }
 

reactos/ntoskrnl/mm/i386/pagepae.c
@@ -2311,13 +2311,11 @@ MiInitPageDirectoryMap(VOID)
 {
    MEMORY_AREA* kernel_map_desc = NULL;
    MEMORY_AREA* hyperspace_desc = NULL;
-   PHYSICAL_ADDRESS BoundaryAddressMultiple;
    PVOID BaseAddress;
    NTSTATUS Status;
 
    DPRINT("MiInitPageDirectoryMap()\n");
 
-   BoundaryAddressMultiple.QuadPart = 0;
    BaseAddress = (PVOID)PAGETABLE_MAP;
    Status = MmCreateMemoryArea(MmGetKernelAddressSpace(),
                                MEMORY_AREA_SYSTEM,
@@ -2327,7 +2325,7 @@ MiInitPageDirectoryMap(VOID)
                                &kernel_map_desc,
                                TRUE,
                                0,
-                               BoundaryAddressMultiple);
+                               PAGE_SIZE);
    if (!NT_SUCCESS(Status))
    {
       ASSERT(FALSE);
@@ -2341,7 +2339,7 @@ MiInitPageDirectoryMap(VOID)
                                &hyperspace_desc,
                                TRUE,
                                0,
-                               BoundaryAddressMultiple);
+                               PAGE_SIZE);
    if (!NT_SUCCESS(Status))
    {
       ASSERT(FALSE);

commit 38455d7927e1756941acbddbbea2d5063b74bc34

[NTOSKRNL] - Do not align the size of a memory area to the allocation granularity, but to PAGE_SIZE. Fixes OllyDbg regression from r61108. CORE-8168 #resolve - Clarify the size calculation in MmCreateMemoryArea - Silence a few DPRINTs
git-svn-id: file:///srv/svn/reactos/trunk@63405 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/ntoskrnl/mm/ARM3/miarm.h
@@ -1100,11 +1100,11 @@ MI_WS_OWNER(IN PEPROCESS Process)
     /* Check if this process is the owner, and that the thread owns the WS */
     if (PsGetCurrentThread()->OwnsProcessWorkingSetExclusive == 0)
     {
-        DPRINT1("Thread: %p is not an owner\n", PsGetCurrentThread());
+        DPRINT("Thread: %p is not an owner\n", PsGetCurrentThread());
     }
     if (KeGetCurrentThread()->ApcState.Process != &Process->Pcb)
     {
-        DPRINT1("Current thread %p is attached to another process %p\n", PsGetCurrentThread(), Process);
+        DPRINT("Current thread %p is attached to another process %p\n", PsGetCurrentThread(), Process);
     }
     return ((KeGetCurrentThread()->ApcState.Process == &Process->Pcb) &&
             ((PsGetCurrentThread()->OwnsProcessWorkingSetExclusive) ||

reactos/ntoskrnl/mm/ARM3/virtual.c
@@ -1291,10 +1291,10 @@ MiGetPageProtection(IN PMMPTE PointerPte)
     }
 
     /* This is software PTE */
-    DPRINT1("Prototype PTE: %lx %p\n", TempPte.u.Hard.PageFrameNumber, Pfn);
-    DPRINT1("VA: %p\n", MiPteToAddress(&TempPte));
-    DPRINT1("Mask: %lx\n", TempPte.u.Soft.Protection);
-    DPRINT1("Mask2: %lx\n", Pfn->OriginalPte.u.Soft.Protection);
+    DPRINT("Prototype PTE: %lx %p\n", TempPte.u.Hard.PageFrameNumber, Pfn);
+    DPRINT("VA: %p\n", MiPteToAddress(&TempPte));
+    DPRINT("Mask: %lx\n", TempPte.u.Soft.Protection);
+    DPRINT("Mask2: %lx\n", Pfn->OriginalPte.u.Soft.Protection);
     return MmProtectToValue[TempPte.u.Soft.Protection];
 }
 

reactos/ntoskrnl/mm/marea.c
@@ -988,6 +988,7 @@ MmCreateMemoryArea(PMMSUPPORT AddressSpace,
 {
    ULONG_PTR tmpLength;
    PMEMORY_AREA MemoryArea;
+   ULONG_PTR EndingAddress;
 
    DPRINT("MmCreateMemoryArea(Type 0x%lx, BaseAddress %p, "
           "*BaseAddress %p, Length %p, AllocationFlags %x, "
@@ -997,7 +998,7 @@ MmCreateMemoryArea(PMMSUPPORT AddressSpace,
 
    if ((*BaseAddress) == 0 && !FixedAddress)
    {
-      tmpLength = (ULONG_PTR)MM_ROUND_UP(Length, Granularity);
+      tmpLength = (ULONG_PTR)MM_ROUND_UP(Length, PAGE_SIZE);
       *BaseAddress = MmFindGap(AddressSpace,
                                tmpLength,
                                Granularity,
@@ -1010,10 +1011,9 @@ MmCreateMemoryArea(PMMSUPPORT AddressSpace,
    }
    else
    {
-      tmpLength = Length + ((ULONG_PTR) *BaseAddress
-                         - (ULONG_PTR) MM_ROUND_DOWN(*BaseAddress, Granularity));
-      tmpLength = (ULONG_PTR)MM_ROUND_UP(tmpLength, Granularity);
-      *BaseAddress = MM_ROUND_DOWN(*BaseAddress, Granularity);
+      EndingAddress = ((ULONG_PTR)*BaseAddress + Length - 1) | (PAGE_SIZE - 1);
+      *BaseAddress = ALIGN_DOWN_POINTER_BY(*BaseAddress, Granularity);
+      tmpLength = EndingAddress + 1 - (ULONG_PTR)*BaseAddress;
 
       if (!MmGetAddressSpaceOwner(AddressSpace) && *BaseAddress < MmSystemRangeStart)
       {