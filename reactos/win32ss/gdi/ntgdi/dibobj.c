reactos/reactos-deprecated-gitsvn-dont-use

commit 7eb7752d8874ec086cd0fd4324d2ea53ae06bef0

[WIN32K] Annotate a number of GDI functions to match public header declarations.
git-svn-id: file:///srv/svn/reactos/trunk@66644 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/win32ss/gdi/ntgdi/dibobj.c
@@ -1002,6 +1002,8 @@ done:
     return ScanLines;
 }
 
+_Success_(return!=0)
+__kernel_entry
 INT
 APIENTRY
 NtGdiGetDIBitsInternal(
@@ -1009,13 +1011,13 @@ NtGdiGetDIBitsInternal(
     _In_ HBITMAP hbm,
     _In_ UINT iStartScan,
     _In_ UINT cScans,
-    _Out_opt_ LPBYTE pjBits,
-    _Inout_ LPBITMAPINFO pbmiUser,
+    _Out_writes_bytes_opt_(cjMaxBits) LPBYTE pjBits,
+    _Inout_ LPBITMAPINFO pbmi,
     _In_ UINT iUsage,
     _In_ UINT cjMaxBits,
     _In_ UINT cjMaxInfo)
 {
-    PBITMAPINFO pbmi;
+    PBITMAPINFO pbmiSafe;
     HANDLE hSecure = NULL;
     INT iResult = 0;
     UINT cjAlloc;
@@ -1037,8 +1039,8 @@ NtGdiGetDIBitsInternal(
     cjAlloc = sizeof(BITMAPV5HEADER) + 256 * sizeof(RGBQUAD);
 
     /* Allocate a buffer the bitmapinfo */
-    pbmi = ExAllocatePoolWithTag(PagedPool, cjAlloc, 'imBG');
-    if (!pbmi)
+    pbmiSafe = ExAllocatePoolWithTag(PagedPool, cjAlloc, 'imBG');
+    if (!pbmiSafe)
     {
         /* Fail */
         return 0;
@@ -1048,8 +1050,8 @@ NtGdiGetDIBitsInternal(
     _SEH2_TRY
     {
         /* Probe and copy the BITMAPINFO */
-        ProbeForRead(pbmiUser, cjMaxInfo, 1);
-        RtlCopyMemory(pbmi, pbmiUser, cjMaxInfo);
+        ProbeForRead(pbmi, cjMaxInfo, 1);
+        RtlCopyMemory(pbmi, pbmi, cjMaxInfo);
     }
     _SEH2_EXCEPT(EXCEPTION_EXECUTE_HANDLER)
     {
@@ -1058,8 +1060,8 @@ NtGdiGetDIBitsInternal(
     _SEH2_END;
 
     /* Check if the header size is large enough */
-    if ((pbmi->bmiHeader.biSize < sizeof(BITMAPCOREHEADER)) ||
-        (pbmi->bmiHeader.biSize > cjMaxInfo))
+    if ((pbmiSafe->bmiHeader.biSize < sizeof(BITMAPCOREHEADER)) ||
+        (pbmiSafe->bmiHeader.biSize > cjMaxInfo))
     {
         goto cleanup;
     }
@@ -1081,7 +1083,7 @@ NtGdiGetDIBitsInternal(
                                    iStartScan,
                                    cScans,
                                    pjBits,
-                                   pbmi,
+                                   pbmiSafe,
                                    iUsage,
                                    cjMaxBits,
                                    cjMaxInfo);
@@ -1093,20 +1095,21 @@ NtGdiGetDIBitsInternal(
         _SEH2_TRY
         {
             /* Copy the data back */
-            cjMaxInfo = min(cjMaxInfo, (UINT)DIB_BitmapInfoSize(pbmi, (WORD)iUsage));
-            ProbeForWrite(pbmiUser, cjMaxInfo, 1);
-            RtlCopyMemory(pbmiUser, pbmi, cjMaxInfo);
+            cjMaxInfo = min(cjMaxInfo, (UINT)DIB_BitmapInfoSize(pbmiSafe, (WORD)iUsage));
+            ProbeForWrite(pbmi, cjMaxInfo, 1);
+            RtlCopyMemory(pbmi, pbmiSafe, cjMaxInfo);
         }
         _SEH2_EXCEPT(EXCEPTION_EXECUTE_HANDLER)
         {
             /* Ignore */
+            (VOID)0;
         }
         _SEH2_END;
     }
 
 cleanup:
     if (hSecure) EngUnsecureMem(hSecure);
-    ExFreePoolWithTag(pbmi, 'imBG');
+    ExFreePoolWithTag(pbmiSafe, 'imBG');
 
     return iResult;
 }

reactos/win32ss/gdi/eng/clip.c
@@ -241,10 +241,10 @@ APIENTRY
 EngDeleteClip(
     _In_ _Post_ptr_invalid_ CLIPOBJ *pco)
 {
-    XCLIPOBJ* Clip = CONTAINING_RECORD(pco, XCLIPOBJ, ClipObj);
-    TRACE("Deleting %p.\n");
-    IntEngFreeClipResources(Clip);
-    EngFreeMem(Clip);
+    XCLIPOBJ* pxco = CONTAINING_RECORD(pco, XCLIPOBJ, ClipObj);
+    TRACE("Deleting %p.\n", pco);
+    IntEngFreeClipResources(pxco);
+    EngFreeMem(pxco);
 }
 
 /*

reactos/win32ss/gdi/eng/eng.h
@@ -11,16 +11,18 @@ NTAPI
 EngAcquireSemaphoreShared(
     IN HSEMAPHORE hsem);
 
-BOOL APIENTRY
-IntEngMaskBlt(SURFOBJ *psoDest,
-              SURFOBJ *psoMask,
-              CLIPOBJ *ClipRegion,
-              XLATEOBJ *DestColorTranslation,
-              XLATEOBJ *SourceColorTranslation,
-              RECTL *DestRect,
-              POINTL *pptlMask,
-              BRUSHOBJ *pbo,
-              POINTL *BrushOrigin);
+BOOL
+APIENTRY
+IntEngMaskBlt(
+    _Inout_ SURFOBJ *psoDest,
+    _In_ SURFOBJ *psoMask,
+    _In_ CLIPOBJ *pco,
+    _In_ XLATEOBJ *pxloDest,
+    _In_ XLATEOBJ *pxloSource,
+    _In_ RECTL *prclDest,
+    _In_ POINTL *pptlMask,
+    _In_ BRUSHOBJ *pbo,
+    _In_ POINTL *pptlBrushOrg);
 
 VOID FASTCALL
 IntEngWindowChanged(

reactos/win32ss/gdi/eng/float.c
@@ -15,14 +15,20 @@
 
 /* FUNCTIONS *****************************************************************/
 
+_Check_return_
+_Success_(return)
+_Kernel_float_restored_
+_At_(*pBuffer, _Kernel_requires_resource_held_(EngFloatState)
+               _Kernel_releases_resource_(EngFloatState))
+ENGAPI
 BOOL
 APIENTRY
 EngRestoreFloatingPointState(
-    PVOID Buffer)
+    _In_reads_(_Inexpressible_(statesize)) PVOID pBuffer)
 {
     NTSTATUS Status;
 
-    Status = KeRestoreFloatingPointState((PKFLOATING_SAVE)Buffer);
+    Status = KeRestoreFloatingPointState((PKFLOATING_SAVE)pBuffer);
     if (!NT_SUCCESS(Status))
     {
         return FALSE;
@@ -31,16 +37,24 @@ EngRestoreFloatingPointState(
     return TRUE;
 }
 
+_Check_return_
+_Success_(((pBuffer != NULL && cjBufferSize != 0) && return == 1) ||
+          ((pBuffer == NULL || cjBufferSize == 0) && return > 0))
+_When_(pBuffer != NULL && cjBufferSize != 0 && return == 1, _Kernel_float_saved_
+    _At_(*pBuffer, _Post_valid_ _Kernel_acquires_resource_(EngFloatState)))
+_On_failure_(_Post_satisfies_(return == 0))
+ENGAPI
 ULONG
 APIENTRY
 EngSaveFloatingPointState(
-    PVOID Buffer,
-    ULONG BufferSize)
+    _At_(*pBuffer, _Kernel_requires_resource_not_held_(EngFloatState))
+    _Out_writes_bytes_opt_(cjBufferSize) PVOID pBuffer,
+    _Inout_ ULONG cjBufferSize)
 {
     KFLOATING_SAVE TempBuffer;
     NTSTATUS Status;
 
-    if ((Buffer == NULL) || (BufferSize == 0))
+    if ((pBuffer == NULL) || (cjBufferSize == 0))
     {
         /* Check for floating point support. */
         Status = KeSaveFloatingPointState(&TempBuffer);
@@ -53,12 +67,12 @@ EngSaveFloatingPointState(
         return(sizeof(KFLOATING_SAVE));
     }
 
-    if (BufferSize < sizeof(KFLOATING_SAVE))
+    if (cjBufferSize < sizeof(KFLOATING_SAVE))
     {
         return(0);
     }
 
-    Status = KeSaveFloatingPointState((PKFLOATING_SAVE)Buffer);
+    Status = KeSaveFloatingPointState((PKFLOATING_SAVE)pBuffer);
     if (!NT_SUCCESS(Status))
     {
         return FALSE;

reactos/win32ss/gdi/eng/inteng.h
@@ -138,12 +138,14 @@ IntEngTransparentBlt(SURFOBJ *Dest,
                      ULONG iTransColor,
                      ULONG Reserved);
 
-BOOL APIENTRY
-IntEngPaint(IN SURFOBJ *Surface,
-            IN CLIPOBJ *ClipRegion,
-            IN BRUSHOBJ *Brush,
-            IN POINTL *BrushOrigin,
-            IN MIX Mix);
+BOOL
+APIENTRY
+IntEngPaint(
+    _In_ SURFOBJ *pso,
+    _In_ CLIPOBJ *pco,
+    _In_ BRUSHOBJ *pbo,
+    _In_ POINTL *pptlBrushOrg,
+    _In_ __in_data_source(USER_MODE) MIX mix);
 
 ULONG APIENTRY
 IntEngSetPointerShape(

reactos/win32ss/gdi/eng/ldevobj.h
@@ -43,8 +43,8 @@ LDEVOBJ_pdmiGetModes(
 PLDEVOBJ
 APIENTRY
 EngLoadImageEx(
-    LPWSTR pwszDriverName,
-    ULONG ldevtype);
+    _In_z_ LPWSTR pwszDriverName,
+    _In_ ULONG ldevtype);
 
 PLDEVOBJ
 NTAPI

reactos/win32ss/gdi/eng/mapping.c
@@ -381,6 +381,12 @@ EngLoadModuleEx(
                           FILE_NON_DIRECTORY_FILE,
                           NULL,
                           0);
+    if (!NT_SUCCESS(Status))
+    {
+        DPRINT1("Failed to open file, hFile=%p, Status=0x%x\n", hFile, Status);
+        EngFreeMem(pFileView);
+        return NULL;
+    }
 
     Status = ZwQueryInformationFile(hFile,
                                     &IoStatusBlock,

reactos/win32ss/gdi/eng/semaphor.c
@@ -17,7 +17,7 @@ EngCreateSemaphore(VOID)
 
     if (!NT_SUCCESS(ExInitializeResourceLite(psem)))
     {
-        ExFreePoolWithTag ( psem, GDITAG_SEMAPHORE );
+        ExFreePoolWithTag(psem, GDITAG_SEMAPHORE );
         return NULL;
     }
 

commit 0df0d2ea749d8d3ea3b99dc1e7bd4118fd404b2f

[WIN32K] Fix a typo introduced in r66644. Fixes icon corruption / testbot regression. CORE-9351 #resolve
git-svn-id: file:///srv/svn/reactos/trunk@66656 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/win32ss/gdi/ntgdi/dibobj.c
@@ -1051,7 +1051,7 @@ NtGdiGetDIBitsInternal(
     {
         /* Probe and copy the BITMAPINFO */
         ProbeForRead(pbmi, cjMaxInfo, 1);
-        RtlCopyMemory(pbmi, pbmi, cjMaxInfo);
+        RtlCopyMemory(pbmiSafe, pbmi, cjMaxInfo);
     }
     _SEH2_EXCEPT(EXCEPTION_EXECUTE_HANDLER)
     {