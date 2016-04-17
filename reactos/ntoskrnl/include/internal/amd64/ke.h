d0ac91f39184348354d9350872e126d6e07c9444
Andrew Cook <ariscop@gmail.com>
2016-04-17 19:47:53 +1000

    Don't take an address in KeSweepICache
    No actual implementation needs it and it causes a pointer-to-int
    in kdapi

--- reactos/ntoskrnl/include/internal/amd64/ke.h
+++ reactos/ntoskrnl/include/internal/amd64/ke.h
@@ -190,14 +190,8 @@ KeFlushProcessTb(VOID)
 
 FORCEINLINE
 VOID
-KeSweepICache(IN PVOID BaseAddress,
-              IN SIZE_T FlushSize)
+KeSweepICache(VOID)
 {
-    //
-    // Always sweep the whole cache
-    //
-    UNREFERENCED_PARAMETER(BaseAddress);
-    UNREFERENCED_PARAMETER(FlushSize);
     __wbinvd();
 }
 
