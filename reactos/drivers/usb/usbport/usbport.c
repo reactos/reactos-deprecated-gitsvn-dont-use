f867a1738d5b2cc1c92c85940207c28934648afe
vgalnt <vgal@rambler.ru>
2016-05-21 02:50:50 +0800

    [USBPORT] Stub DriverEntry(), USBPORT_RegisterUSBPortDriver() and USBPORT_GetHciMn().

--- reactos/drivers/usb/usbport/usbport.c
+++ reactos/drivers/usb/usbport/usbport.c
@@ -1,6 +1,36 @@
 #include "usbport.h"
 
-#define NDEBUG
+//#define NDEBUG
 #include <debug.h>
 
+ULONG
+NTAPI
+USBPORT_GetHciMn(VOID)
+{
+    return 0;
+}
 
+NTSTATUS
+NTAPI
+USBPORT_RegisterUSBPortDriver(PDRIVER_OBJECT DriverObject,
+                              ULONG Version,
+                              PUSBPORT_REGISTRATION_PACKET RegPacket)
+{
+    NTSTATUS Status;
+
+    DPRINT("USBPORT_RegisterUSBPortDriver: DriverObject - %p, Version - %p, RegPacket - %p\n",
+           DriverObject,
+           Version,
+           RegPacket);
+
+    Status = STATUS_SUCCESS;
+    return Status;
+}
+
+NTSTATUS
+NTAPI
+DriverEntry(PDRIVER_OBJECT DriverObject,
+            PUNICODE_STRING RegistryPath)
+{
+    return STATUS_SUCCESS;
+}
