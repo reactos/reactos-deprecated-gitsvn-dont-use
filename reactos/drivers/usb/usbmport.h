54c308ecce4080def278f2bbd0a266dd2055cbcf
vgalnt <vgal@rambler.ru>
2016-05-21 02:11:40 +0800

    [USB] Define stucture _USBPORT_REGISTRATION_PACKET for registration MiniPort driver.

--- reactos/drivers/usb/usbmport.h
+++ reactos/drivers/usb/usbmport.h
@@ -1,6 +1,8 @@
 #ifndef USBMPORT_H__
 #define USBMPORT_H__
 
-
+typedef struct _USBPORT_REGISTRATION_PACKET {
+  ULONG Version; // Type: 1 - OHCI, 2 - UHCI, 3 - EHCI
+} USBPORT_REGISTRATION_PACKET, *PUSBPORT_REGISTRATION_PACKET;
 
 #endif /* USBMPORT_H__ */
