5f4b149cfccd7b461d9f815bcf34c00ea2b67550
vgalnt <vgal@rambler.ru>
2016-05-27 16:04:38 +0800

    [USBPORT] Start USBPORT_PdoQueryInterface().

--- reactos/drivers/usb/usbport/iface.c
+++ reactos/drivers/usb/usbport/iface.c
@@ -2,3 +2,48 @@
 
 //#define NDEBUG
 #include <debug.h>
+
+static
+NTSTATUS
+NTAPI
+USBPORT_PdoQueryInterface(PDEVICE_OBJECT FdoDevice,
+                          PDEVICE_OBJECT PdoDevice,
+                          PIRP Irp)
+{
+    PIO_STACK_LOCATION IoStack = IoGetCurrentIrpStackLocation(Irp);
+    PUSB_BUS_INTERFACE_HUB_V5 InterfaceHub;
+    PUSB_BUS_INTERFACE_USBDI_V2 InterfaceDI;
+    UNICODE_STRING GuidBuffer;
+    NTSTATUS Status;
+
+    DPRINT("USBPORT_PdoQueryInterface: ... \n");
+
+    if (IsEqualGUIDAligned((REFGUID)(IoStack->Parameters.QueryInterface.InterfaceType),
+                           &USB_BUS_INTERFACE_HUB_GUID))
+    {
+        return STATUS_SUCCESS;
+    }
+    else if (IsEqualGUIDAligned(IoStack->Parameters.QueryInterface.InterfaceType,
+                                &USB_BUS_INTERFACE_USBDI_GUID))
+    {
+        return STATUS_SUCCESS;
+    }
+    else
+    {
+        // Convert GUID to string
+        Status = RtlStringFromGUID(IoStack->Parameters.QueryInterface.InterfaceType,
+                                   &GuidBuffer);
+
+        if (NT_SUCCESS(Status))
+        {
+            // Print interface
+            DPRINT1("HandleQueryInterface UNKNOWN INTERFACE GUID: %wZ Version %x\n",
+                    &GuidBuffer,
+                    IoStack->Parameters.QueryInterface.Version);
+            
+            RtlFreeUnicodeString(&GuidBuffer); // Free GUID buffer
+        }
+    }
+
+    return STATUS_NOT_SUPPORTED;
+}
