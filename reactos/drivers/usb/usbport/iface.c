e3d8a0da82ba8061a5305316ec891caadb6d98b9
vgalnt <vgal@rambler.ru>
2016-05-27 16:12:19 +0800

    [USBPORT] USBPORT_PdoQueryInterface(): Port Driver Interfaces.

--- reactos/drivers/usb/usbport/iface.c
+++ reactos/drivers/usb/usbport/iface.c
@@ -21,6 +21,61 @@ USBPORT_PdoQueryInterface(PDEVICE_OBJECT FdoDevice,
     if (IsEqualGUIDAligned((REFGUID)(IoStack->Parameters.QueryInterface.InterfaceType),
                            &USB_BUS_INTERFACE_HUB_GUID))
     {
+        // Get request parameters
+        InterfaceHub = (PUSB_BUS_INTERFACE_HUB_V5)IoStack->Parameters.QueryInterface.Interface;
+        InterfaceHub->Version = IoStack->Parameters.QueryInterface.Version;
+
+        // Check version
+        if (IoStack->Parameters.QueryInterface.Version >= 6)
+        {
+            DPRINT1("USB_BUS_INTERFACE_HUB_GUID version %x not supported!\n",
+                    IoStack->Parameters.QueryInterface.Version);
+
+            return STATUS_NOT_SUPPORTED; // Version not supported
+        }
+
+        // Interface version 0
+        InterfaceHub->Size = IoStack->Parameters.QueryInterface.Size;
+        InterfaceHub->BusContext = (PVOID)PdoDevice;
+
+        InterfaceHub->InterfaceReference = USBI_InterfaceReference;
+        InterfaceHub->InterfaceDereference = USBI_InterfaceDereference;
+
+        // Interface version 1
+        if (IoStack->Parameters.QueryInterface.Version >= 1)
+        {
+            InterfaceHub->CreateUsbDevice = USBHI_CreateUsbDevice;
+            InterfaceHub->InitializeUsbDevice = USBHI_InitializeUsbDevice;
+            InterfaceHub->GetUsbDescriptors = USBHI_GetUsbDescriptors;
+            InterfaceHub->RemoveUsbDevice = USBHI_RemoveUsbDevice;
+            InterfaceHub->RestoreUsbDevice = USBHI_RestoreUsbDevice;
+            InterfaceHub->QueryDeviceInformation = USBHI_QueryDeviceInformation;
+        }
+
+        // Interface version 2
+        if (IoStack->Parameters.QueryInterface.Version >= 2)
+        {
+            InterfaceHub->GetControllerInformation = USBHI_GetControllerInformation;
+            InterfaceHub->ControllerSelectiveSuspend = USBHI_ControllerSelectiveSuspend;
+            InterfaceHub->GetExtendedHubInformation = USBHI_GetExtendedHubInformation;
+            InterfaceHub->GetRootHubSymbolicName = USBHI_GetRootHubSymbolicName;
+            InterfaceHub->GetDeviceBusContext = USBHI_GetDeviceBusContext;
+            InterfaceHub->Initialize20Hub = USBHI_Initialize20Hub;
+        }
+
+        // Interface version 3
+        if (IoStack->Parameters.QueryInterface.Version >= 3)
+            InterfaceHub->RootHubInitNotification = USBHI_RootHubInitNotification;
+
+        // Interface version 4
+        if (IoStack->Parameters.QueryInterface.Version >= 4)
+            InterfaceHub->FlushTransfers = USBHI_FlushTransfers;
+
+        // Interface version 5
+        if (IoStack->Parameters.QueryInterface.Version >= 5)
+            InterfaceHub->SetDeviceHandleData = USBHI_SetDeviceHandleData;
+
+        // Request completed
         return STATUS_SUCCESS;
     }
     else if (IsEqualGUIDAligned(IoStack->Parameters.QueryInterface.InterfaceType,
