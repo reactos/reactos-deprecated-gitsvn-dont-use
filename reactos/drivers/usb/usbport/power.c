534434451b6ee5877cbeef50b0aee2add73ed867
vgalnt <vgal@rambler.ru>
2016-10-03 02:28:41 +0800

    [USBPORT] USBPORT_AdjustDeviceCapabilities() for PDO (Root Hub).

--- reactos/drivers/usb/usbport/power.c
+++ reactos/drivers/usb/usbport/power.c
@@ -38,3 +38,45 @@ USBPORT_IdleNotification(IN PDEVICE_OBJECT PdoDevice,
     Status = STATUS_NOT_SUPPORTED;
     return Status;
 }
+
+VOID
+NTAPI
+USBPORT_AdjustDeviceCapabilities(IN PDEVICE_OBJECT FdoDevice,
+                                 IN PDEVICE_OBJECT PdoDevice)
+{
+    PUSBPORT_DEVICE_EXTENSION FdoExtension;
+    PUSBPORT_RHDEVICE_EXTENSION PdoExtension;
+    PDEVICE_CAPABILITIES Capabilities;
+
+    DPRINT("USBPORT_AdjustDeviceCapabilities: ... \n");
+
+    FdoExtension = (PUSBPORT_DEVICE_EXTENSION)FdoDevice->DeviceExtension;
+    PdoExtension = (PUSBPORT_RHDEVICE_EXTENSION)PdoDevice->DeviceExtension;
+    Capabilities = &PdoExtension->Capabilities;
+
+    RtlCopyMemory(Capabilities, &FdoExtension->Capabilities, sizeof(DEVICE_CAPABILITIES));
+
+    Capabilities->DeviceD1 = FALSE;
+    Capabilities->DeviceD2 = TRUE;
+
+    Capabilities->Removable = FALSE;
+    Capabilities->UniqueID = FALSE;
+
+    Capabilities->WakeFromD0 = TRUE;
+    Capabilities->WakeFromD1 = FALSE;
+    Capabilities->WakeFromD2 = TRUE;
+    Capabilities->WakeFromD3 = FALSE;
+
+    Capabilities->Address = 0;
+    Capabilities->UINumber = 0;
+
+    if (Capabilities->SystemWake == PowerSystemUnspecified)
+        Capabilities->SystemWake = PowerSystemWorking;
+
+    Capabilities->DeviceWake = PowerDeviceD2;
+
+    Capabilities->DeviceState[PowerSystemSleeping1] = PowerDeviceD3;
+    Capabilities->DeviceState[PowerSystemSleeping2] = PowerDeviceD3;
+    Capabilities->DeviceState[PowerSystemSleeping3] = PowerDeviceD3;
+    Capabilities->DeviceState[PowerSystemHibernate] = PowerDeviceD3;
+}
