d495e76b5016fee0368da5bb031628f0d90b344a
vgalnt <vgal@rambler.ru>
2016-10-03 02:35:28 +0800

    [USBPORT] Continue USBPORT_FdoPower(). Add stubs for USBPORT_HcWake(), USBPORT_DevicePowerState() and USBPORT_SystemPowerState().

--- reactos/drivers/usb/usbport/power.c
+++ reactos/drivers/usb/usbport/power.c
@@ -16,12 +16,88 @@ USBPORT_PdoPower(IN PDEVICE_OBJECT PdoDevice,
 
 NTSTATUS
 NTAPI
+USBPORT_HcWake(IN PDEVICE_OBJECT FdoDevice,
+               IN PIRP Irp)
+{
+    DPRINT("USBPORT_HcWake: ... \n");
+    ASSERT(FALSE);
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+NTAPI
+USBPORT_DevicePowerState(IN PDEVICE_OBJECT FdoDevice,
+                         IN PIRP Irp)
+{
+    DPRINT("USBPORT_DevicePowerState: ... \n");
+    ASSERT(FALSE);
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+NTAPI
+USBPORT_SystemPowerState(IN PDEVICE_OBJECT FdoDevice,
+                         IN PIRP Irp)
+{
+    DPRINT("USBPORT_SystemPowerState: ... \n");
+    ASSERT(FALSE);
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+NTAPI
 USBPORT_FdoPower(IN PDEVICE_OBJECT FdoDevice,
                  IN PIRP Irp)
 {
-    DPRINT("USBPORT_FdoPower: FIXME \n");
-    ASSERT(FALSE);
-    return 0;
+    PUSBPORT_DEVICE_EXTENSION FdoExtension;
+    PIO_STACK_LOCATION IoStack;
+    NTSTATUS Status;
+
+    DPRINT("USBPORT_FdoPower: ... \n");
+
+    FdoExtension = (PUSBPORT_DEVICE_EXTENSION)FdoDevice->DeviceExtension;
+    IoStack = IoGetCurrentIrpStackLocation(Irp);
+
+    switch (IoStack->MinorFunction)
+    {
+      case IRP_MN_WAIT_WAKE:
+          DPRINT("USBPORT_FdoPower: IRP_MN_WAIT_WAKE\n");
+          Status = USBPORT_HcWake(FdoDevice, Irp);
+          return Status;
+
+      case IRP_MN_POWER_SEQUENCE:
+          DPRINT("USBPORT_FdoPower: IRP_MN_POWER_SEQUENCE\n");
+          break;
+
+      case IRP_MN_SET_POWER:
+          DPRINT("USBPORT_FdoPower: IRP_MN_SET_POWER\n");
+          if (IoStack->Parameters.Power.Type == DevicePowerState)
+          {
+              Status = USBPORT_DevicePowerState(FdoDevice, Irp);
+          }
+          else
+          {
+              Status = USBPORT_SystemPowerState(FdoDevice, Irp);
+          }
+      
+          if (Status != STATUS_PENDING)
+              break;
+
+          return Status;
+
+      case IRP_MN_QUERY_POWER:
+          DPRINT("USBPORT_FdoPower: IRP_MN_QUERY_POWER\n");
+          Irp->IoStatus.Status = STATUS_SUCCESS;
+          break;
+
+      default:
+          DPRINT1("USBPORT_FdoPower: unknown IRP_MN_POWER!\n");
+          break;
+    }
+
+    IoCopyCurrentIrpStackLocationToNext(Irp);
+    PoStartNextPowerIrp(Irp);
+    return PoCallDriver(FdoExtension->CommonExtension.LowerDevice, Irp);
 }
 
 NTSTATUS
