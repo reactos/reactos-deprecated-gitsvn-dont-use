b549819794ebfd8be44b8ff44164bcf60cb6d53c
vgalnt <vgal@rambler.ru>
2016-10-11 21:45:56 +0800

    [USBPORT] Add debug option NDEBUG_USBPORT_QUEUE and DPRINT_QUEUE.

--- reactos/drivers/usb/usbport/usbdebug.h
+++ reactos/drivers/usb/usbport/usbdebug.h
@@ -88,6 +88,23 @@
 
     #endif
 
+    #ifndef NDEBUG_USBPORT_QUEUE
+
+        #define DPRINT_QUEUE(fmt, ...) do { \
+            if (DbgPrint("(%s:%d) " fmt, __RELFILE__, __LINE__, ##__VA_ARGS__))  \
+                DbgPrint("(%s:%d) DbgPrint() failed!\n", __RELFILE__, __LINE__); \
+        } while (0)
+
+    #else
+
+#if defined(_MSC_VER)
+        #define DPRINT_QUEUE   __noop
+#else
+        #define DPRINT_QUEUE(...) do { if(0) { DbgPrint(__VA_ARGS__); } } while(0)
+#endif
+
+    #endif
+
 #else /* not DBG */
 
 #if defined(_MSC_VER)
@@ -96,12 +113,14 @@
     #define DPRINT_URB    __noop
     #define DPRINT_INT    __noop
     #define DPRINT_TIMER    __noop
+    #define DPRINT_QUEUE    __noop
 #else
     #define DPRINT_MINIPORT(...) do { if(0) { DbgPrint(__VA_ARGS__); } } while(0)
     #define DPRINT_CORE(...) do { if(0) { DbgPrint(__VA_ARGS__); } } while(0)
     #define DPRINT_URB(...) do { if(0) { DbgPrint(__VA_ARGS__); } } while(0)
     #define DPRINT_INT(...) do { if(0) { DbgPrint(__VA_ARGS__); } } while(0)
     #define DPRINT_TIMER(...) do { if(0) { DbgPrint(__VA_ARGS__); } } while(0)
+    #define DPRINT_QUEUE(...) do { if(0) { DbgPrint(__VA_ARGS__); } } while(0)
 #endif /* _MSC_VER */
 
 #endif /* not DBG */
