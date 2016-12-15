037cdcbfc9b97ce2005f3e669b0fd0772d8b0371
vgalnt <vgal@rambler.ru>
2016-12-16 07:17:21 +0800

    [USBHUB] Add switchs for USBH_(F|P)doPower().

--- reactos/drivers/usb/usbhub_new/power.c
+++ reactos/drivers/usb/usbhub_new/power.c
@@ -9,14 +9,41 @@ USBH_FdoPower(IN PUSBHUB_FDO_EXTENSION HubExtension,
               IN PIRP Irp,
               IN UCHAR Minor)
 {
+    NTSTATUS Status;
+
     DPRINT("USBH_FdoPower: HubExtension - %p, Irp - %p, Minor - %x\n",
            HubExtension,
            Irp,
            Minor);
 
-    DbgBreakPoint();
+    switch (Minor)
+    {
+      case IRP_MN_WAIT_WAKE:
+          DPRINT("USBPORT_FdoPower: IRP_MN_WAIT_WAKE\n");
+          break;
 
-    return 0;
+      case IRP_MN_POWER_SEQUENCE:
+          DPRINT("USBPORT_FdoPower: IRP_MN_POWER_SEQUENCE\n");
+          break;
+
+      case IRP_MN_SET_POWER:
+          DPRINT("USBPORT_FdoPower: IRP_MN_SET_POWER\n");
+          break;
+
+      case IRP_MN_QUERY_POWER:
+          DPRINT("USBPORT_FdoPower: IRP_MN_QUERY_POWER\n");
+          break;
+
+      default:
+          DPRINT1("USBPORT_FdoPower: unknown IRP_MN_POWER!\n");
+          break;
+    }
+
+    IoCopyCurrentIrpStackLocationToNext(Irp);
+    PoStartNextPowerIrp(Irp);
+    Status = PoCallDriver(HubExtension->LowerDevice, Irp);
+
+    return Status;
 }
 
 NTSTATUS
@@ -25,12 +52,44 @@ USBH_PdoPower(IN PUSBHUB_PORT_PDO_EXTENSION PortExtension,
               IN PIRP Irp,
               IN UCHAR Minor)
 {
+    NTSTATUS Status=0;
+
     DPRINT("USBH_FdoPower: PortExtension - %p, Irp - %p, Minor - %x\n",
            PortExtension,
            Irp,
            Minor);
 
-    DbgBreakPoint();
+    switch (Minor)
+    {
+      case IRP_MN_WAIT_WAKE:
+          DPRINT("USBPORT_PdoPower: IRP_MN_WAIT_WAKE\n");
+          PoStartNextPowerIrp(Irp);
+          break;
 
-    return 0;
+      case IRP_MN_POWER_SEQUENCE:
+          DPRINT("USBPORT_PdoPower: IRP_MN_POWER_SEQUENCE\n");
+          PoStartNextPowerIrp(Irp);
+          break;
+
+      case IRP_MN_SET_POWER:
+          DPRINT("USBPORT_PdoPower: IRP_MN_SET_POWER\n");
+          PoStartNextPowerIrp(Irp);
+          break;
+
+      case IRP_MN_QUERY_POWER:
+          DPRINT("USBPORT_PdoPower: IRP_MN_QUERY_POWER\n");
+          PoStartNextPowerIrp(Irp);
+          break;
+
+      default:
+          DPRINT1("USBPORT_PdoPower: unknown IRP_MN_POWER!\n");
+          PoStartNextPowerIrp(Irp);
+          break;
+    }
+
+    Irp->IoStatus.Status = Status;
+    Irp->IoStatus.Information = 0;
+    IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+    return Status;
 }
