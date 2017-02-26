reactos/reactos-deprecated-gitsvn-dont-use

commit fa42e8ba678fc5cdf468af66cd83b7164b395e7c

[ACPICA] - Update to version 20161222 CORE-12833
git-svn-id: file:///srv/svn/reactos/trunk@73918 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/drivers/bus/acpi/osl.c
@@ -929,6 +929,16 @@ AcpiOsSignal (
 }
 
 ACPI_STATUS
+AcpiOsEnterSleep(
+    UINT8 SleepState,
+    UINT32 RegaValue,
+    UINT32 RegbValue)
+{
+    DPRINT1("Attempt to enter sleep. Aborting.\n");
+    return AE_CTRL_TERMINATE;
+}
+
+ACPI_STATUS
 AcpiOsGetLine(
     char *Buffer,
     UINT32 BufferLength,

commit 29b0733fc0c24b5a4ac1fbb0d49109959898bbc2

[ACPI] - We do support sleep states (namely, S5), so don't force aborting sleep transitions. Fixes ACPI shutdown broken by r73918. CORE-12833
git-svn-id: file:///srv/svn/reactos/trunk@73919 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/drivers/bus/acpi/osl.c
@@ -934,8 +934,8 @@ AcpiOsEnterSleep(
     UINT32 RegaValue,
     UINT32 RegbValue)
 {
-    DPRINT1("Attempt to enter sleep. Aborting.\n");
-    return AE_CTRL_TERMINATE;
+    DPRINT1("Entering sleep state S%u.\n", SleepState);
+    return AE_OK;
 }
 
 ACPI_STATUS