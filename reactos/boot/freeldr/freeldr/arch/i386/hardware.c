reactos/reactos-deprecated-gitsvn-dont-use

commit 629e3b20e910956ec96071520322da527d2046c7

[FREELDR] Take in account the boot disk in the number of detected disks. It allows booting without a hard disk (e.g cdrom/floppy alone with an USB storage device)
git-svn-id: file:///srv/svn/reactos/trunk@59079 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/boot/freeldr/freeldr/arch/i386/hwdisk.c
@@ -313,8 +313,6 @@ HwInitializeBiosDisks(VOID)
         memset((PVOID) DISKREADBUFFER, 0xcd, 512);
     }
     DiskReportError(TRUE);
-    TRACE("BIOS reports %d harddisk%s\n",
-          (int)DiskCount, (DiskCount == 1) ? "": "s");
 
     /* Get the drive we're booting from */
     MachDiskGetBootPath(BootPath, sizeof(BootPath));
@@ -349,8 +347,12 @@ HwInitializeBiosDisks(VOID)
         reactos_disk_count++;
 
         FsRegisterDevice(BootPath, &DiskVtbl);
+        DiskCount++;
     }
 
     PcBiosDiskCount = DiskCount;
+    TRACE("BIOS reports %d harddisk%s\n",
+          (int)DiskCount, (DiskCount == 1) ? "": "s");
+
     return DiskCount != 0;
 }

commit 313cfa9097cf95edac724f2b2bb6a1fd2af6fb4f

[FREELDR]: Report the correct number of BIOS (hard) disk drives detected, that is, without any possible extra boot drive that went enumerated afterwards. This fixes the list of disks enumerated in HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\MultifunctionAdapter\0\DiskController\0\DiskPeripheral\ and we now behave in this regards similarly to Windows 2003 & co. I thank Lesan Ilie for reporting & testing, and Serge Gautherie and Peter Hater for reviewing. This fixes a bug introduced in commit r59079. CORE-13131
git-svn-id: file:///srv/svn/reactos/trunk@74515 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/boot/freeldr/freeldr/arch/i386/hardware.c
@@ -35,6 +35,8 @@ DBG_DEFAULT_CHANNEL(HWDETECT);
 #define LATCH (CLOCK_TICK_RATE / HZ)
 
 static unsigned int delay_count = 1;
+
+/* Used for BIOS disks pre-enumeration performed when detecting the boot devices in InitializeBootDevices() */
 extern UCHAR PcBiosDiskCount;
 
 /* This function is slightly different in its PC and XBOX versions */

reactos/boot/freeldr/freeldr/arch/i386/hwdisk.c
@@ -44,6 +44,7 @@ extern ARC_DISK_SIGNATURE_EX reactos_arc_disk_info[];
 
 static CHAR Hex[] = "0123456789abcdef";
 
+/* Data cache for BIOS disks pre-enumeration */
 UCHAR PcBiosDiskCount = 0;
 static CHAR PcDiskIdentifier[32][20];
 
@@ -323,8 +324,8 @@ PcInitializeBootDevices(VOID)
         }
         if (!Changed)
         {
-            TRACE("BIOS reports success for disk %d but data didn't change\n",
-                  (int)DiskCount);
+            TRACE("BIOS reports success for disk %d (0x%02X) but data didn't change\n",
+                  (int)DiskCount, DriveNumber);
             break;
         }
 
@@ -340,6 +341,8 @@ PcInitializeBootDevices(VOID)
         memset(DiskReadBuffer, 0xcd, DiskReadBufferSize);
     }
     DiskReportError(TRUE);
+
+    PcBiosDiskCount = DiskCount;
     TRACE("BIOS reports %d harddisk%s\n",
           (int)DiskCount, (DiskCount == 1) ? "" : "s");
 
@@ -384,12 +387,9 @@ PcInitializeBootDevices(VOID)
         reactos_disk_count++;
 
         FsRegisterDevice(BootPath, &DiskVtbl);
-        DiskCount++;
+        DiskCount++; // This is not accounted for in the number of pre-enumerated BIOS drives!
+        TRACE("Additional boot drive detected: 0x%02X\n", (int)FrldrBootDrive);
     }
-
-    PcBiosDiskCount = DiskCount;
-    TRACE("BIOS reports %d harddisk%s\n",
-          (int)DiskCount, (DiskCount == 1) ? "": "s");
 
     return (DiskCount != 0);
 }